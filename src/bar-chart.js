5d40d0392d28509120eea6973adaa54aa7202c86
Nick Zhu <nick.qi.zhu@gmail.com>
2012-07-02 19:23:48 -0400

    added x,y axis to bar chart with auto range round

--- src/bar-chart.js
+++ src/bar-chart.js
@@ -2,13 +2,34 @@ dc.barChart = function(selector) {
 
     var chart = dc.baseMixin({});
 
-    var margin = {top: 10, right: 50, bottom: 20, left: 5};
+    var margin = {top: 10, right: 50, bottom: 30, left: 20};
+
+    var x;
+    var y = d3.scale.linear().range([100, 0]);
 
     chart.render = function() {
         chart.resetSvg();
 
         if (chart.dataAreSet()) {
-            var topG = chart.generateTopLevelG().attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var g = chart.generateTopLevelG().attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            g.selectAll("rect")
+                .data(chart.group().all())
+                .enter().append("rect");
+
+            x.rangeRound([0, (chart.width() - margin.left - margin.right)]);
+            var axisX = d3.svg.axis().scale(x).orient("bottom");
+            g.append("g")
+                .attr("class", "axis x")
+                .attr("transform", "translate(" + margin.left + "," + (chart.height() - margin.bottom) + ")")
+                .call(axisX);
+
+            y.domain([0, chart.group().top(1)[0].value]).rangeRound([0, chart.height() - margin.top - margin.bottom]);
+            var axisY = d3.svg.axis().scale(y).ticks(5).orient("left");
+            g.append("g")
+                .attr("class", "axis y")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .call(axisY);
         }
     };
 
@@ -16,7 +37,19 @@ dc.barChart = function(selector) {
         if (!arguments.length) return margin;
         margin = m;
         return chart;
-    }
+    };
+
+    chart.x = function(_) {
+        if (!arguments.length) return x;
+        x = _;
+        return chart;
+    };
+
+    chart.y = function(_) {
+        if (!arguments.length) return y;
+        y = _;
+        return chart;
+    };
 
     dc.registerChart(chart);
 
--- test/bar-chart-test.js
+++ test/bar-chart-test.js
@@ -5,8 +5,8 @@ var assert = require('assert');
 
 var suite = vows.describe('Bar chart');
 
-var width = 400;
-var height = 150;
+var width = 1100;
+var height = 200;
 
 suite.addBatch({
     'bar chart generation': {
@@ -15,6 +15,7 @@ suite.addBatch({
             var chart = dc.barChart("#bar-chart");
             chart.dimension(dateDimension).group(dateGroup)
                 .width(width).height(height)
+                .x(d3.time.scale().domain([new Date("Sun Jan 01 2012 00:00:00 GMT-0400 (EDT)"), new Date("Mon Dec 31 2012 00:00:00 GMT-0400 (EDT)")]));
             chart.render();
             return chart;
         },
@@ -42,11 +43,31 @@ suite.addBatch({
         'margin should be set': function(chart) {
             assert.isNotNull(chart.margins());
         },
+        'x can be set': function(chart) {
+            assert.isTrue(chart.x() != undefined);
+        },
+        'x range round is auto calculated based on width': function(chart) {
+            assert.equal(chart.x().range()[0], 0);
+            assert.equal(chart.x().range()[1], 1030);
+        },
+        'y can be set': function(chart) {
+            assert.isTrue(chart.y() != undefined);
+        },
+        'y range round is auto calculated based on height': function(chart) {
+            assert.equal(chart.y().range()[0], 0);
+            assert.equal(chart.y().range()[1], 160);
+        },
         'root g should be created': function(chart) {
             assert.isFalse(chart.select("svg g").empty());
         },
         'root g should be translated to left corner': function(chart) {
-            assert.equal(chart.select("svg g").attr("transform"), "translate(5,10)");
+            assert.equal(chart.select("svg g").attr("transform"), "translate(20,10)");
+        },
+        'axis x should be placed at the bottom': function(chart) {
+            assert.equal(chart.select("svg g g.x").attr("transform"), "translate(20,170)");
+        },
+        'axis y should be placed on the left': function(chart) {
+            assert.equal(chart.select("svg g g.y").attr("transform"), "translate(20,10)");
         },
         teardown: function(topic){
             resetAllFilters();
