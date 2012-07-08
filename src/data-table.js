18b311e8172b08dd811fd74d5b9bfbeaf4271cb2
Nick Zhu <nick.qi.zhu@gmail.com>
2012-07-07 23:34:07 -0400

    implemented basic data table widget

--- src/data-table.js
+++ src/data-table.js
@@ -5,21 +5,40 @@ dc.dataTable = function(selector) {
     var columns = [];
 
     chart.render = function() {
+        var div = chart.root()
+            .selectAll("div")
+            .data(chart.dimension().top(size));
+
+        var divEnter = div.enter()
+            .append("div")
+            .attr("class", "row");
+
+        for (var i = 0; i < columns.length; ++i) {
+            var f = columns[i];
+            divEnter.append("span")
+                .attr("class", "column " + i)
+                .text(function(d) {
+                    return f(d);
+                });
+        }
+
+        div.exit().remove();
+
         return chart;
     };
 
-    chart.redraw = function(){
+    chart.redraw = function() {
         return chart.render();
     };
 
-    chart.size = function(s){
-        if(!arguments.length) return size;
+    chart.size = function(s) {
+        if (!arguments.length) return size;
         size = s;
         return chart;
     }
 
-    chart.columns = function(_){
-        if(!arguments.length) return columns;
+    chart.columns = function(_) {
+        if (!arguments.length) return columns;
         columns = _;
         return chart;
     }
--- test/data-table-test.js
+++ test/data-table-test.js
@@ -10,8 +10,8 @@ suite.addBatch({
         topic: function() {
             var div = d3.select("body").append("div").attr("id", "data-table");
             var chart = dc.dataTable("#data-table")
-                .dimension(data)
-                .group(groupAll)
+                .dimension(dateDimension)
+                .group(dateGroup)
                 .size(3)
                 .columns([function(d){return d.id;}, function(d){return d.status;}]);
             chart.render();
@@ -24,7 +24,10 @@ suite.addBatch({
             assert.isTrue(dc.hasChart(chart));
         },
         'should have id column created':function(chart) {
-            console.log(chart.selectAll("span"));
+            assert.isFalse(chart.selectAll("span.0").empty());
+        },
+        'should have status column created':function(chart) {
+            assert.isFalse(chart.selectAll("span.1").empty());
         },
         'teardown': function() {
             resetAllFilters();
