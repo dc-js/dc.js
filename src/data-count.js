bf90f5aacb916f82e946efa557f2378f3b25e86c
Nick Zhu <nick.qi.zhu@gmail.com>
2012-07-09 17:07:11 -0400

    implemented total count

--- src/data-count.js
+++ src/data-count.js
@@ -1,8 +1,12 @@
 dc.dataCount = function(selector){
     var chart = dc.baseChart({});
 
+    chart.render = function(){
+        chart.selectAll(".total-count").text(chart.dimension().size());
+
+        return chart;
+    };
 
     dc.registerChart(chart);
-
     return chart.anchor(selector);
 };
