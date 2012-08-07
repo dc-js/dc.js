3858e743fb869c0d76a5b0904cfaeb24293d5e68
Nick Zhu <nick.qi.zhu@gmail.com>
2012-08-06 21:30:23 -0400

    add support for grid line rendering
     - all sub chart of coordinate grid chart now support grid line rendering
     - renderHorizontalGridLines() and renderVerticalGridLines() functions are added to turn grid lines on/off
     - grid line rendering works with both auto ticks and custom ticks

--- dc.js
+++ dc.js
@@ -589,6 +589,9 @@ dc.baseChart = function(_chart) {
 };
 dc.coordinateGridChart = function(_chart) {
     var DEFAULT_Y_AXIS_TICKS = 5;
+    var GRID_LINE_CLASS = "grid-line";
+    var HORIZONTAL_CLASS = "horizontal";
+    var VERTICAL_CLASS = "vertical";
 
     _chart = dc.baseChart(_chart);
 
@@ -610,6 +613,9 @@ dc.coordinateGridChart = function(_chart) {
     var _filter;
     var _brush = d3.svg.brush();
     var _round;
+
+    var _renderHorizontalGridLine = false;
+    var _renderVerticalGridLine = false;
 
     _chart.generateG = function() {
         _g = _chart.svg().append("g")
@@ -654,7 +660,30 @@ dc.coordinateGridChart = function(_chart) {
             .attr("class", "axis x")
             .attr("transform", "translate(" + _chart.margins().left + "," + _chart.xAxisY() + ")")
             .call(_xAxis);
+
+        renderVerticalGridLines(g);
     };
+
+    function renderVerticalGridLines(g) {
+        if (_renderVerticalGridLine) {
+            g.selectAll("g." + VERTICAL_CLASS).remove();
+
+            var ticks = _xAxis.tickValues()?_xAxis.tickValues():_x.ticks(_xAxis.ticks()[0]);
+
+            var gridLineG = g.append("g")
+                .attr("class", GRID_LINE_CLASS + " " + VERTICAL_CLASS)
+                .attr("transform", "translate(" + _chart.yAxisX() + "," + _chart.margins().top + ")");
+
+            for (var i = 0; i < ticks.length; ++i) {
+                var tick = ticks[i];
+                gridLineG.append("line")
+                    .attr("x1", _x(tick))
+                    .attr("y1", _chart.xAxisY() - _chart.margins().top)
+                    .attr("x2", _x(tick))
+                    .attr("y2", 0);
+            }
+        }
+    }
 
     _chart.xAxisY = function() {
         return (_chart.height() - _chart.margins().bottom);
@@ -683,8 +712,49 @@ dc.coordinateGridChart = function(_chart) {
 
         g.append("g")
             .attr("class", "axis y")
-            .attr("transform", "translate(" + _chart.margins().left + "," + _chart.margins().top + ")")
+            .attr("transform", "translate(" + _chart.yAxisX() + "," + _chart.margins().top + ")")
             .call(_yAxis);
+
+
+        renderHorizontalGridLines(g);
+    };
+
+    function renderHorizontalGridLines(g) {
+        if (_renderHorizontalGridLine) {
+            g.selectAll("g." + HORIZONTAL_CLASS).remove();
+
+            var ticks = _yAxis.tickValues()?_yAxis.tickValues():_y.ticks(_yAxis.ticks()[0]);
+
+            var gridLineG = g.append("g")
+                .attr("class", GRID_LINE_CLASS + " " + HORIZONTAL_CLASS)
+                .attr("transform", "translate(" + _chart.yAxisX() + "," + _chart.margins().top + ")");
+
+            for (var i = 0; i < ticks.length; ++i) {
+                if (i == 0) continue;
+                var tick = ticks[i];
+                gridLineG.append("line")
+                    .attr("x1", 1)
+                    .attr("y1", _y(tick))
+                    .attr("x2", _chart.xAxisLength())
+                    .attr("y2", _y(tick));
+            }
+        }
+    }
+
+    _chart.renderHorizontalGridLines = function(_) {
+        if (!arguments.length) return _renderHorizontalGridLine;
+        _renderHorizontalGridLine = _;
+        return _chart;
+    };
+
+    _chart.renderVerticalGridLines = function(_){
+        if(!arguments.length) return _renderVerticalGridLine;
+        _renderVerticalGridLine = _;
+        return _chart;
+    };
+
+    _chart.yAxisX = function() {
+        return _chart.margins().left;
     };
 
     _chart.y = function(_) {
