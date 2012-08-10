587be86a923cbd692d0696715850540811c649f1
Nick Zhu <nick.qi.zhu@gmail.com>
2012-08-10 12:34:19 -0400

    [issue #22] add support to turn off centering in bar chart

--- dc.js
+++ dc.js
@@ -1406,6 +1406,8 @@ dc.barChart = function(parent, chartGroup) {
 
     var _chart = dc.stackableChart(dc.coordinateGridChart({}));
 
+    var _centering = true;
+
     _chart.transitionDuration(500);
 
     _chart.plotData = function() {
@@ -1441,8 +1443,10 @@ dc.barChart = function(parent, chartGroup) {
 
         // update
         dc.transition(bars, _chart.transitionDuration())
+            .attr("x", function(data, dataIndex) {
+                return barX(this, data, groupIndex, dataIndex);
+            })
             .attr("y", function(data, dataIndex) {
-
                 return barY(this, data, dataIndex);
             })
             .attr("height", function(data) {
@@ -1469,7 +1473,10 @@ dc.barChart = function(parent, chartGroup) {
 
     function barX(bar, data, groupIndex, dataIndex) {
         setGroupIndexToBar(bar, groupIndex);
-        return _chart.x()(_chart.keyRetriever()(data)) + _chart.margins().left - barWidth(data)/2;
+        var position = _chart.x()(_chart.keyRetriever()(data)) + _chart.margins().left;
+        if(_centering)
+            position = position - barWidth(data)/2;
+        return position;
     }
 
     function getGroupIndexFromBar(bar) {
@@ -1496,6 +1503,12 @@ dc.barChart = function(parent, chartGroup) {
         } else {
             bars.classed(dc.constants.DESELECTED_CLASS, false);
         }
+    };
+
+    _chart.centering = function(_){
+        if(!arguments.length) return _centering;
+        _centering = _;
+        return _chart;
     };
 
     return _chart.anchor(parent, chartGroup);
