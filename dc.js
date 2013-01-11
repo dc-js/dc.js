42bea4b53152ad63fe1a1e4be73f31733d3b0dbf
Nick Zhu <nick.qi.zhu@gmail.com>
2013-01-10 21:24:59 -0500

    fine tuned bar title setting performance

--- dc.js
+++ dc.js
@@ -2558,7 +2558,7 @@ dc.bubbleChart = function(parent, chartGroup) {
 
         _chart.r().range([_chart.MIN_RADIUS, _chart.xAxisLength() * _chart.maxBubbleRelativeSize()]);
 
-        var bubbleG = _chart.g().selectAll("g." + _chart.BUBBLE_NODE_CLASS)
+        var bubbleG = _chart.chartBodyG().selectAll("g." + _chart.BUBBLE_NODE_CLASS)
             .data(_chart.group().all());
 
         renderNodes(bubbleG);
