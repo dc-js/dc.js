37a0be04266e271e95e028e6af481031a747ee83
Nick Zhu <nick.qi.zhu@gmail.com>
2013-06-22 16:37:42 -0400

    start v1.5 development

--- src/package.js
+++ src/package.js
@@ -3,9 +3,9 @@ require("./core");
 require("util").puts(JSON.stringify({
   "name": "dc",
   "version": dc.version,
-  "description": "A multi-dimensional charting built to work natively with crossfilter rendered using d3.js ",
+  "description": "A multi-dimensional charting library built to work natively with crossfilter and rendered using d3.js ",
   "keywords": ["visualization", "svg", "animation", "canvas", "chart", "dimensional"],
-  "homepage": "http://nickqizhu.github.com/dc.js/",
+  "homepage": "http://nickqizhu.github.io/dc.js/",
   "author": {"name": "Nick Zhu", "url": "http://nzhu.blogspot.ca/"},
   "repository": {"type": "git", "url": "https://github.com/NickQiZhu/dc.js.git"},
   "dependencies": {
