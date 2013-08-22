dbbc59d0c3363aec3e3abd7b24839414ef9c5aa5
Nick Zhu <nick.qi.zhu@gmail.com>
2013-08-22 12:59:44 -0400

    start v1.6 dev

--- src/core.js
+++ src/core.js
@@ -12,7 +12,7 @@
  *  limitations under the License.
  */
 dc = {
-    version: "1.5.0",
+    version: "1.6.0",
     constants : {
         CHART_CLASS: "dc-chart",
         DEBUG_GROUP_CLASS: "debug",
