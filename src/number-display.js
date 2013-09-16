8895efe9b9373e58301b6b481236f9acddbb7f7d
Jacob Rideout <jacob@jacobrideout.net>
2013-09-15 21:56:11 -0400

    redo number chart

--- src/number-display.js
+++ src/number-display.js
@@ -1,69 +1,49 @@
-/*  dc.numberDisplay example:
- *    <div id="total-sales-chart" class="widget">
- *        <strong>Total Sales</strong>
- *        <h2>$<span class="number-display"></span></h2>
- *    </div>
- */
-
 dc.numberDisplay = function (parent, chartGroup) {
+    var SPAN_CLASS = 'number-display';
+    var _formatNumber = d3.format(".2s");
+    var _chart = dc.baseChart({});
 
-  var _chart = dc.baseChart({});
+    _chart.dimension({}); // dummy dimension to remove warnings
 
-  _chart.doRender = function () {
-    var newValue = _chart.group().top(1)[0].value;
+    _chart.value = function () {
+         var valObj = _chart.group().all && _chart.group().all()[0] || _chart.group().value();
+         return _chart.valueAccessor()(valObj);
+    };
 
-    _chart.selectAll(".number-display")
-    .transition()
-    .duration(750)
+    _chart.doRender = function () {
+        var newValue = _chart.value(),
+            span     = _chart.selectAll("."+SPAN_CLASS);
 
-    .tween("text", function (d, i, a) {
-      var interp = d3.interpolate(dehumanize(this.textContent), newValue);
-      return function (t) {
-        this.textContent = humanize(Math.round(interp(t)));
-      };
-    });
+        if(span.empty())
+            span = span.data([0])
+                .enter()
+                .append("span")
+                .attr("class", SPAN_CLASS);
 
-    return _chart;
-  };
+        span.transition()
+            .duration(250)
+            .ease('quad-out-in')
+            .tween("text", function () {
+                var interp = d3.interpolateNumber(this.lastValue || 0, newValue);
+                this.lastValue = newValue;
+                return function (t) {
+                    this.textContent = _chart.formatNumber()(interp(t));
+                };
+            });
 
-  _chart.doRedraw = function () {
-    return _chart.doRender();
-  };
+        return _chart;
+    };
 
+    _chart.doRedraw = function(){
+        return _chart.doRender();
+    };
 
-  function humanize(value) {
-    if (value >= 1000000) {
-      var val = (value / 1000000);
-      if (val.toFixed(1).length > 3)
-        return val.toFixed(0) + "M";
-      else
-        return val.toFixed(1) + "M";
-    }
-    if (value >= 1000) {
-      var val = (value / 1000);
-      if (val.toFixed(1).length > 3)
-        return val.toFixed(0) + "K";
-      else
-        return val.toFixed(1) + "K";
-    }
+    _chart.formatNumber = function (_) {
+        if (!arguments.length) return _formatNumber;
+        _formatNumber = _;
+        return _chart;
+    };
 
-    return value + "";
-  }
-
-  function dehumanize(text) {
-    if (text === "" || text === undefined)
-      return 0;
-
-    var num = parseFloat(text);
-
-    if (text.indexOf("K") !== -1)
-      return num * 1000;
-
-    if (text.indexOf("M") !== -1)
-      return num * 1000000;
-
-    return num;
-  }
-
-  return _chart.anchor(parent, chartGroup);
+    return _chart.anchor(parent, chartGroup);
 };
+
