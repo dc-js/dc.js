8a7f0701ca3c9870a4f2dec2ed4a7ca4cc6f9d41
Jacob Rideout <jacob@jacobrideout.net>
2013-10-13 21:26:44 -0400

    Synced from from master branch.

--- js/dc.js
+++ js/dc.js
@@ -1570,7 +1570,7 @@ dc.coordinateGridChart = function (_chart) {
     var _clipPadding = 0;
 
     _chart.title(function (d) {
-        return d.data.key + ": " + d.data.value;
+        return _chart.keyAccessor()(d.data) + ": " + _chart.valueAccessor()(d.data);
     });
 
     _chart.rescale = function () {
@@ -4511,6 +4511,7 @@ dc.compositeChart = function (parent, chartGroup) {
 dc.seriesChart = function (parent, chartGroup) {
     var _chart = dc.compositeChart(parent, chartGroup);
 
+    var _charts = {};
     var _chartFunction = dc.lineChart;
     var _seriesAccessor;
 
@@ -4519,34 +4520,51 @@ dc.seriesChart = function (parent, chartGroup) {
 
     dc.override(_chart, "plotData", function () {
         dc.deregisterAllCharts(_chart.anchorName());
+        var keep = [];
         var children = d3.nest().key(_seriesAccessor).entries(_chart.data())
             .map(function(sub,i) {
-                var subChart = _chartFunction(_chart,_chart.anchorName());
+                var subChart = _charts[sub.key] || _chartFunction(_chart,_chart.anchorName());
+                _charts[sub.key] = subChart;
+                keep.push(sub.key);
                 return subChart
                     .group({all:d3.functor(sub.values)}, sub.key)
                     .keyAccessor(_chart.keyAccessor())
                     .valueAccessor(_chart.valueAccessor())
                     .colorCalculator(function() {return subChart.colors()(sub.key);});
         });
+        Object.keys(_charts)
+            .filter(function(c) {return keep.indexOf(c) === -1;})
+            .map(function(c) {return _charts[c].resetSvg();});
         _chart._compose(children);
         _chart._plotData();
     });
 
+    function clearChart(c) {
+        return _charts[c].resetSvg();
+    }
+
+    function resetChildren() {
+        Object.keys(_charts).map(clearChart);
+        _charts = [];
+    }
+
     _chart.chart = function(_) {
         if (!arguments.length) return _chartFunction;
         _chartFunction = _;
+        resetChildren();
         return _chart;
     };
 
     _chart.seriesAccessor = function(_) {
         if (!arguments.length) return _seriesAccessor;
         _seriesAccessor = _;
+        resetChildren();
         return _chart;
     };
 
     // make compose private
     _chart._compose = _chart.compose;
-    //delete _chart.compose;
+    delete _chart.compose;
 
     return _chart;
 };
