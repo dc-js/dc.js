447ec79f54adff69b73150af044f3eb1429292fc
Jacob Rideout <jacob@jacobrideout.net>
2013-10-13 21:23:31 -0400

    remove old series sub charts

--- src/series-chart.js
+++ src/series-chart.js
@@ -4,6 +4,7 @@
 dc.seriesChart = function (parent, chartGroup) {
     var _chart = dc.compositeChart(parent, chartGroup);
 
+    var _charts = {};
     var _chartFunction = dc.lineChart;
     var _seriesAccessor;
 
@@ -12,34 +13,51 @@ dc.seriesChart = function (parent, chartGroup) {
 
     dc.override(_chart, "plotData", function () {
         dc.deregisterAllCharts(_chart.anchorName());
+        var keep = [];
         var children = d3.nest().key(_seriesAccessor).entries(_chart.data())
             .map(function(sub,i) {
-                var subChart = _chartFunction(_chart,_chart.anchorName());
+                var subChart = _charts[sub.key] || _chartFunction(_chart,_chart.anchorName());
+                _charts[sub.key] = subChart;
+                keep.push(sub.key);
                 return subChart
                     .group({all:d3.functor(sub.values)}, sub.key)
                     .keyAccessor(_chart.keyAccessor())
                     .valueAccessor(_chart.valueAccessor())
                     .colorCalculator(function() {return subChart.colors()(sub.key);});
         });
+        Object.keys(_charts)
+            .filter(function(c) {return keep.indexOf(c) === -1;})
+            .map(function(c) {return _charts[c].resetSvg();});
         _chart._compose(children);
         _chart._plotData();
     });
 
+    function clearChart(c) {
+        return _charts[c].resetSvg();
+    }
+
+    function resetChildren() {
+        Object.keys(_charts).map(clearChart);
+        _charts = [];
+    }
+
     _chart.chart = function(_) {
         if (!arguments.length) return _chartFunction;
         _chartFunction = _;
+        resetChildren();
         return _chart;
     };
 
     _chart.seriesAccessor = function(_) {
         if (!arguments.length) return _seriesAccessor;
         _seriesAccessor = _;
+        resetChildren();
         return _chart;
     };
 
     // make compose private
     _chart._compose = _chart.compose;
-    //delete _chart.compose;
+    delete _chart.compose;
 
     return _chart;
 };
