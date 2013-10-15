0992951d71b4c5b180831b95b3b727cc81d6fa7c
Nick Zhu <nick.qi.zhu@gmail.com>
2013-10-15 16:03:58 -0400

    implement scatterplot update logic

--- test/scatterplot-test.js
+++ test/scatterplot-test.js
@@ -14,23 +14,22 @@ function buildChart(id, xdomain) {
 
     d3.select("body").append("div").attr("id", id);
     var chart = dc.scatterPlot("#" + id);
-    chart.dimension(dateDimension).group(dateGroup)
+    chart.dimension(dateDimension).group(dateIdSumGroup)
         .width(width).height(height)
         .x(d3.time.scale().domain(xdomain))
-        .transitionDuration(0)
-        .xUnits(d3.time.days);
+        .transitionDuration(0);
     chart.render();
     return chart;
 }
 
 function symbol_by_index(chart, i) {
-    return d3.select(chart.selectAll('path.dc-symbol')[0][ i]);
+    return d3.select(chart.selectAll('circle.symbol')[0][i]);
 }
 
 suite.addBatch({
     'render': {
         topic: function () {
-            return buildChart('scatter-plot');
+            return buildChart('scatter-plot-new');
         },
 
         'should create svg': function (chart) {
@@ -38,13 +37,35 @@ suite.addBatch({
         },
 
         'should create correct number of symbols': function (chart) {
-            assert.equal(chart.group().all().length, chart.selectAll('path.dc-symbol').size());
+            assert.equal(chart.group().all().length, chart.selectAll('circle.symbol').size());
         },
 
         'should correctly place symbols': function (chart) {
             assert.equal("translate(166.8013698630137,140)", symbol_by_index(chart, 0).attr("transform"));
-            assert.equal("translate(209.37671232876713,140)", symbol_by_index(chart, 3).attr("transform"));
-            assert.equal("translate(255.40410958904107,70)", symbol_by_index(chart, 5).attr("transform"));
+            assert.equal("translate(209.37671232876713,114)", symbol_by_index(chart, 3).attr("transform"));
+            assert.equal("translate(255.40410958904107,44)", symbol_by_index(chart, 5).attr("transform"));
+        }
+    },
+
+    teardown: function (topic) {
+        resetAllFilters();
+        resetBody();
+    }
+});
+
+suite.addBatch({
+    'update': {
+        topic: function () {
+            var chart = buildChart('scatter-plot-update');
+            valueDimension.filter(66);
+            chart.render();
+            return chart;
+        },
+
+        'should correctly place symbols': function (chart) {
+            assert.equal("translate(182.91095890410958,96)", symbol_by_index(chart, 1).attr("transform"));
+            assert.equal("translate(209.37671232876713,149)", symbol_by_index(chart, 3).attr("transform"));
+            assert.equal("translate(255.40410958904107,149)", symbol_by_index(chart, 5).attr("transform"));
         }
     },
 
--- web/js/dc.js
+++ web/js/dc.js
@@ -5531,16 +5531,28 @@ dc.capped = function (_chart) {
 dc.scatterPlot = function (parent, chartGroup) {
     var _chart = dc.coordinateGridChart({});
 
-    _chart.plotData = function(){
-        _chart.chartBodyG().selectAll("path.dc-symbol")
-                .data(_chart.data())
+    var _locator = function (d) {
+        return "translate(" + _chart.x()(_chart.keyAccessor()(d)) + "," + _chart.y()(_chart.valueAccessor()(d)) + ")";
+    };
+
+    _chart.plotData = function () {
+        var symbols = _chart.chartBodyG().selectAll("circle.symbol")
+            .data(_chart.data());
+
+        symbols
             .enter()
-            .append("path")
-            .attr("class", "dc-symbol")
-            .attr("transform", function(d){
-                return "translate("+_chart.x()(_chart.keyAccessor()(d))+","+_chart.y()(_chart.valueAccessor()(d))+")";
-            })
-            .attr("d", d3.svg.symbol());
+        .append("circle")
+            .attr("class", "symbol")
+            .attr("transform", _locator);
+
+        dc.transition(symbols, _chart.transitionDuration(), function (symbols) {
+            symbols.attr("transform", _locator)
+                .attr("r", 3);
+        });
+
+        dc.transition(symbols.exit(), _chart.transitionDuration(), function (symbols) {
+            symbols.attr("r", 0).remove();
+        });
     };
 
     return _chart.anchor(parent, chartGroup);
