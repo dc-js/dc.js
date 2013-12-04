3bf307ee840f74d2147eb7d47852035cccf1b584
Matt Hull <matt.hull@lacunasolutionstn.com>
2013-12-04 15:32:03 -0600

    added logger with debug and warn methods

---
+++ src/logger.js
@@ -0,0 +1,27 @@
+dc.logger = {};
+
+dc.logger.enableDebugLog = false;
+
+dc.logger.warn = function (msg) {
+    if (console) {
+        if (console.warn) {
+            console.warn(msg);
+        } else if (console.log) {
+            console.log(msg);
+        }
+    }
+
+    return dc.logger;
+};
+
+dc.logger.debug = function (msg) {
+    if (dc.logger.enableDebugLog && console) {
+        if (console.debug) {
+            console.debug(msg);
+        } else if (console.log) {
+            console.log(msg);
+        }
+    }
+
+    return dc.logger;
+};
