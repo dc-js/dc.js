fb8b7e13cd92ad67659fa43cbbf1aa5c04d5cb0e
Gordon Woodhull <gordon@woodhull.com>
2014-01-28 01:16:03 -0500

    use group key rather than key accessor for bubble chart join
    fixes #492

--- src/bubble-chart.js
+++ src/bubble-chart.js
@@ -64,7 +64,7 @@ dc.bubbleChart = function(parent, chartGroup) {
         _chart.r().range([_chart.MIN_RADIUS, _chart.xAxisLength() * _chart.maxBubbleRelativeSize()]);
 
         var bubbleG = _chart.chartBodyG().selectAll("g." + _chart.BUBBLE_NODE_CLASS)
-            .data(_chart.data(),_chart.keyAccessor());
+            .data(_chart.data(), function (d) { return d.key; });
 
         renderNodes(bubbleG);
 
