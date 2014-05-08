6f4df0df7b43253c07d39d7bcacea2602dfe8011
Gordon Woodhull <gordon@woodhull.com>
2014-05-08 16:55:07 -0400

    fix node module line
    Conflicts:
    	dc.min.js
    	dc.min.js.map

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ return dc;}
 if(typeof define === "function" && define.amd) {
   define(["d3"], _dc);
 } else if(typeof module === "object" && module.exports) {
-  module.exports = d3;
+  module.exports = _dc(d3);
 } else {
   this.dc = _dc(d3);
 }
