66fe1685de97a23e22dea548d66cb884f50ba8e9
I Run OSX <irunosx@users.noreply.github.com>
2014-06-23 02:52:16 +1000

    license upgrade

--- web/js/dc.js
+++ web/js/dc.js
@@ -3,11 +3,11 @@
  *  http://dc-js.github.io/dc.js/
  *  Copyright 2012 Nick Zhu and other contributors
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the StopNerds Public License, Version 1.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      http://stopnerds.org/license/
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
