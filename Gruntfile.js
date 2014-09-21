18857887e8586672b7d88c1a61bab38d631fb63f
Gordon Woodhull <gordon@woodhull.com>
2014-09-21 16:01:00 -0400

    generate index.html for examples directory
    using grunt-fileindex

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,6 +176,41 @@ module.exports = function (grunt) {
                 ]
             }
         },
+        fileindex: {
+            'examples-listing': {
+                options: {
+                    format: function(list, options, dest){
+                        var examples = list.sort().map(function(entry) {
+                            return entry.replace(/.*examples\//, "");
+                        }).filter(function(e) { return e!="index.html"; });
+                        var rows = [];
+                        for(var i=0; i<examples.length; i += 5) {
+                            var cols = [];
+                            for(var j=0; j<5; ++j) {
+                                if(i+j>=examples.length) break;
+                                var fname = examples[i+j];
+                                cols.push("    <td><a href='" + fname + "'>" + fname + "</a></td>");
+                            }
+                            rows.push("  <tr>\n" + cols.join("\n") + "\n<tr>");
+                        }
+                        var body = "<table class='table'>\n" + rows.join("\n") + "\n</table>";
+                        return ["<html><head><title>Index of dc.js examples</title>",
+                                '<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"></head>',
+                                '<body><div class="container">',
+                                '<h2>Examples of using dc.js</h2>',
+                                '<p>An attempt to present a simple example of each chart type.</p>',
+                                '<p>Source <a href="http://https://github.com/dc-js/dc.js/tree/master/web/examples">here</a>.</p>',
+                                body,
+                                "</div></body></html>"].join("\n");
+                    },
+                    absolute: true
+                },
+                files: [
+                    {dest: 'web/examples/index.html', src: ['web/examples/*.html']}
+                ]
+            }
+        },
+
         'gh-pages': {
             options: {
                 base: 'web',
@@ -221,6 +256,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-sed');
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-debug-task');
+    grunt.loadNpmTasks('grunt-fileindex');
 
     // custom tasks
     grunt.registerMultiTask('emu', 'Documentation extraction by emu.', function() {
