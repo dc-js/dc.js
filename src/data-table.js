03b8ede9677fbee188a40aa2e0831a59cc0b9464
Gordon Woodhull <gordon@woodhull.com>
2014-11-30 08:45:47 -0500

    use function identity comparison instead of Function.name
    @monostop, a tiny change here -
    IE11 does not yet support the ES6 feature Function.name
    function identity comparison is well defined, though

--- src/data-table.js
+++ src/data-table.js
@@ -144,7 +144,7 @@ dc.dataTable = function (parent, chartGroup) {
 
     function nestEntries() {
         var entries;
-        if (_order.name === 'd3_ascending') {
+        if (_order === d3.ascending) {
             entries = _chart.dimension().bottom(_size);
         } else {
             entries = _chart.dimension().top(_size);
