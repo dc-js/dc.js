7d7196422d87f4fc322d388c23642e807cfc1df7
Gordon Woodhull <gordon@woodhull.com>
2014-12-15 15:18:14 -0500

    propagate transition duration to d3.behavior.zoom object
    d3 3.5 introduces smooth transitions for double-click zooming
    
    this broke some tests, and it seems like (unless someone requests
    otherwise) the transition duration should be controlled by the chart's
    transitionDuration
    
    so this is partly just to disable those transitions for tests, but also
    because it seems consistent

--- dc.js
+++ dc.js
@@ -2965,7 +2965,8 @@ dc.coordinateGridMixin = function (_chart) {
         _hasBeenMouseZoomable = true;
         _zoom.x(_chart.x())
             .scaleExtent(_zoomScale)
-            .size([_chart.width(), _chart.height()]);
+            .size([_chart.width(), _chart.height()])
+            .duration(_chart.transitionDuration());
         _chart.root().call(_zoom);
     };
 
