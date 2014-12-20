bfd6960193a73468aded7b0391f0bf9324c80f9a
Gordon Woodhull <gordon@woodhull.com>
2014-12-20 12:36:33 -0500

    bump version to 2.0.0-alpha.4

--- web/js/dc.js
+++ web/js/dc.js
@@ -1,5 +1,5 @@
 /*!
- *  dc 2.0.0-alpha.3
+ *  dc 2.0.0-alpha.4
  *  http://dc-js.github.io/dc.js/
  *  Copyright 2012 Nick Zhu and other contributors
  *
@@ -20,7 +20,7 @@
 'use strict';
 
 /**
-#### Version 2.0.0-alpha.3
+#### Version 2.0.0-alpha.4
 The entire dc.js library is scoped under the **dc** name space. It does not introduce anything else
 into the global name space.
 #### Function Chaining
@@ -41,7 +41,7 @@ that are chainable d3 objects.)
 /*jshint -W062*/
 /*jshint -W079*/
 var dc = {
-    version: '2.0.0-alpha.3',
+    version: '2.0.0-alpha.4',
     constants: {
         CHART_CLASS: 'dc-chart',
         DEBUG_GROUP_CLASS: 'debug',
