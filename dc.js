af788e5620188b056a0925af591ee5c5d2b396da
Gordon Woodhull <gordon@woodhull.com>
2014-12-20 12:57:07 -0500

    fixed browserify for bower & npm
    cherry-picking @vprus's fix from master (#702/#704)
    tests are still massively broken and won't be fixed!

--- dc.js
+++ dc.js
@@ -1,5 +1,5 @@
 /*!
- *  dc 1.7.2
+ *  dc 1.7.3
  *  http://dc-js.github.io/dc.js/
  *  Copyright 2012 Nick Zhu and other contributors
  *
@@ -20,7 +20,7 @@
 'use strict';
 
 /**
-#### Version 1.7.2
+#### Version 1.7.3
 
 The entire dc.js library is scoped under **dc** name space. It does not introduce anything else into the global
 name space.
@@ -57,7 +57,7 @@ The API references will highlight the fact if a particular function is not chain
 
 **/
 var dc = {
-    version: "1.7.2",
+    version: "1.7.3",
     constants: {
         CHART_CLASS: "dc-chart",
         DEBUG_GROUP_CLASS: "debug",
@@ -5402,10 +5402,17 @@ return dc;}
     if(typeof define === "function" && define.amd) {
         define(["d3", "crossfilter"], _dc);
     } else if(typeof module === "object" && module.exports) {
-        // When using window global, window.crossfilter is a function
-        // When using require, the value will be an object with 'crossfilter'
-        // field, so we need to access it here.
-        module.exports = _dc(require('d3'), require('crossfilter').crossfilter);
+        var _d3 = require('d3');
+        var _crossfilter = require('crossfilter');
+        // When using npm + browserify, 'crossfilter' is a function,
+        // since package.json specifies index.js as main function, and it
+        // does special handling. When using bower + browserify,
+        // there's no main in bower.json (in fact, there's no bower.json),
+        // so we need to fix it.
+        if (typeof _crossfilter !== "function") {
+            _crossfilter = _crossfilter.crossfilter;
+        }
+        module.exports = _dc(_d3, _crossfilter);
     } else {
         this.dc = _dc(d3, crossfilter);
     }
