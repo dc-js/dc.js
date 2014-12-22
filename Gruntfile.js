f6dfd9ec5d7bb571fc8cb6af240481c6117c9d7a
Gordon Woodhull <gordon@woodhull.com>
2014-12-22 11:12:35 -0500

    build dc.js file but not docs when testing
    fixes #799
    closes #808

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,9 +380,9 @@ module.exports = function (grunt) {
     grunt.registerTask('docs', ['build', 'copy', 'emu', 'toc', 'markdown', 'docco']);
     grunt.registerTask('web', ['docs', 'gh-pages']);
     grunt.registerTask('server', ['docs', 'fileindex', 'jasmine:specs:build', 'connect:server', 'watch:jasmine']);
-    grunt.registerTask('test', ['docs', 'jasmine:specs', 'test-stock-example', 'shell:hooks']);
-    grunt.registerTask('test-browserify', ['docs', 'browserify', 'jasmine:browserify']);
-    grunt.registerTask('coverage', ['docs', 'jasmine:coverage']);
+    grunt.registerTask('test', ['build', 'jasmine:specs', 'test-stock-example', 'shell:hooks']);
+    grunt.registerTask('test-browserify', ['build', 'browserify', 'jasmine:browserify']);
+    grunt.registerTask('coverage', ['build', 'jasmine:coverage']);
     grunt.registerTask('ci', ['test', 'jasmine:specs:build', 'connect:server', 'saucelabs-jasmine']);
     grunt.registerTask('ci-pull', ['test', 'jasmine:specs:build', 'connect:server']);
     grunt.registerTask('lint', ['build', 'jshint', 'jscs']);
