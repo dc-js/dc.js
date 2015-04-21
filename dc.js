fda18e19756ac16498bebf9293a8adc60212905c
Gordon Woodhull <gordon@woodhull.com>
2015-04-21 15:56:02 -0400

    chartGroup() don't reregister if a child chart
    for #834
    fixes #775
    
    @mtraynham, hopefully this makes a bit more sense. i wonder if this is
    really the only place where we need to know whether a chart is a child
    chart

--- dc.js
+++ dc.js
@@ -663,6 +663,7 @@ dc.baseMixin = function (_chart) {
     var _anchor;
     var _root;
     var _svg;
+    var _isChild;
 
     var _minWidth = 200;
     var _defaultWidth = function (element) {
@@ -949,11 +950,13 @@ dc.baseMixin = function (_chart) {
         if (dc.instanceOfChart(a)) {
             _anchor = a.anchor();
             _root = a.root();
+            _isChild = true;
         } else {
             _anchor = a;
             _root = d3.select(_anchor);
             _root.classed(dc.constants.CHART_CLASS, true);
             dc.registerChart(_chart, chartGroup);
+            _isChild = false;
         }
         _chartGroup = chartGroup;
         return _chart;
@@ -1658,9 +1661,13 @@ dc.baseMixin = function (_chart) {
         if (!arguments.length) {
             return _chartGroup;
         }
-        dc.deregisterChart(_chart, _chartGroup);
+        if (!_isChild) {
+            dc.deregisterChart(_chart, _chartGroup);
+        }
         _chartGroup = _;
-        dc.registerChart(_chart, _chartGroup);
+        if (!_isChild) {
+            dc.registerChart(_chart, _chartGroup);
+        }
         return _chart;
     };
 
