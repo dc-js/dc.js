3770b2a46b788a73a31e82ec3f33bf818bd7fac0
Gordon Woodhull <gordon@woodhull.com>
2015-04-21 21:06:52 -0400

    skip regression test
    it is hanging on develop locally. it is randomly dying on travis.
    it burns a lot of developer time. it will disappear entirely soon.

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,7 +373,7 @@ module.exports = function (grunt) {
     grunt.registerTask('docs', ['build', 'copy', 'emu', 'toc', 'markdown', 'docco']);
     grunt.registerTask('web', ['docs', 'gh-pages']);
     grunt.registerTask('server', ['docs', 'fileindex', 'jasmine:specs:build', 'connect:server', 'watch:jasmine']);
-    grunt.registerTask('test', ['build', 'jasmine:specs', 'test-stock-example', 'shell:hooks']);
+    grunt.registerTask('test', ['build', 'jasmine:specs', 'shell:hooks']);
     grunt.registerTask('test-browserify', ['build', 'browserify', 'jasmine:browserify']);
     grunt.registerTask('coverage', ['build', 'jasmine:coverage']);
     grunt.registerTask('ci', ['test', 'jasmine:specs:build', 'connect:server', 'saucelabs-jasmine']);
