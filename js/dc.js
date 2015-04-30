0f5f5578215fd47971f9566d9d183ea17ef70ce4
Gordon Woodhull <gordon@woodhull.com>
2015-04-30 16:07:19 -0400

    Synced from from master branch.

--- js/dc.js
+++ js/dc.js
@@ -1,7 +1,8 @@
 /*!
- *  dc 2.1.0-dev
+ *  dc 2.0.0-beta.9
  *  http://dc-js.github.io/dc.js/
- *  Copyright 2012 Nick Zhu and other contributors
+ *  Copyright 2012-2015 Nick Zhu & the dc.js Developers
+ *  https://github.com/dc-js/dc.js/blob/master/AUTHORS
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,12 +16,11 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 (function() { function _dc(d3, crossfilter) {
 'use strict';
 
 /**
-#### Version 2.1.0-dev
+#### Version 2.0.0-beta.9
 The entire dc.js library is scoped under the **dc** name space. It does not introduce anything else
 into the global name space.
 #### Function Chaining
@@ -41,7 +41,7 @@ that are chainable d3 objects.)
 /*jshint -W062*/
 /*jshint -W079*/
 var dc = {
-    version: '2.1.0-dev',
+    version: '2.0.0-beta.9',
     constants: {
         CHART_CLASS: 'dc-chart',
         DEBUG_GROUP_CLASS: 'debug',
@@ -663,6 +663,7 @@ dc.baseMixin = function (_chart) {
     var _anchor;
     var _root;
     var _svg;
+    var _isChild;
 
     var _minWidth = 200;
     var _defaultWidth = function (element) {
@@ -949,11 +950,13 @@ dc.baseMixin = function (_chart) {
         if (dc.instanceOfChart(a)) {
             _anchor = a.anchor();
             _root = a.root();
+            _isChild = true;
         } else {
             _anchor = a;
             _root = d3.select(_anchor);
             _root.classed(dc.constants.CHART_CLASS, true);
             dc.registerChart(_chart, chartGroup);
+            _isChild = false;
         }
         _chartGroup = chartGroup;
         return _chart;
@@ -1658,7 +1661,13 @@ dc.baseMixin = function (_chart) {
         if (!arguments.length) {
             return _chartGroup;
         }
+        if (!_isChild) {
+            dc.deregisterChart(_chart, _chartGroup);
+        }
         _chartGroup = _;
+        if (!_isChild) {
+            dc.registerChart(_chart, _chartGroup);
+        }
         return _chart;
     };
 
@@ -3260,8 +3269,7 @@ dc.stackMixin = function (_chart) {
 
     function flattenStack() {
         var valueses = _chart.data().map(function (layer) { return layer.values; });
-        var flattened = Array.prototype.concat.apply([], valueses);
-        return _chart._computeOrderedGroups(flattened);
+        return Array.prototype.concat.apply([], valueses);
     }
 
     _chart.xAxisMin = function () {
@@ -3336,7 +3344,9 @@ dc.stackMixin = function (_chart) {
     });
 
     _chart._ordinalXDomain = function () {
-        return flattenStack().map(dc.pluck('x'));
+        var flat = flattenStack().map(dc.pluck('data'));
+        var ordered = _chart._computeOrderedGroups(flat);
+        return ordered.map(_chart.keyAccessor());
     };
 
     _chart.colorAccessor(function (d) {
@@ -5781,6 +5791,7 @@ dc.compositeChart = function (parent, chartGroup) {
             child.transitionDuration(_chart.transitionDuration());
             child.brushOn(_chart.brushOn());
             child.renderTitle(_chart.renderTitle());
+            child.elasticX(_chart.elasticX());
         }
 
         return g;
@@ -7847,11 +7858,6 @@ dc.heatMap = function (parent, chartGroup) {
 
     var _cols;
     var _rows;
-    var _colOrdering = d3.ascending;
-    var _rowOrdering = d3.ascending;
-    var _colScale = d3.scale.ordinal();
-    var _rowScale = d3.scale.ordinal();
-
     var _xBorderRadius = DEFAULT_BORDER_RADIUS;
     var _yBorderRadius = DEFAULT_BORDER_RADIUS;
 
@@ -7939,55 +7945,47 @@ dc.heatMap = function (parent, chartGroup) {
         return _chart._filter(dc.filters.TwoDimensionalFilter(filter));
     });
 
+    function uniq(d, i, a) {
+        return !i || a[i - 1] !== d;
+    }
+
     /**
      #### .rows([values])
      Gets or sets the values used to create the rows of the heatmap, as an array. By default, all
-     the values will be fetched from the data using the value accessor.
+     the values will be fetched from the data using the value accessor, and they will be sorted in
+     ascending order.
      **/
 
     _chart.rows = function (_) {
-        if (!arguments.length) {
+        if (arguments.length) {
+            _rows = _;
+            return _chart;
+        }
+        if (_rows) {
             return _rows;
         }
-        _rows = _;
-        return _chart;
-    };
-
-    /**
-     #### .rowOrdering([orderFunction])
-     Get or set an accessor to order the rows.  Default is d3.ascending.
-     */
-    _chart.rowOrdering = function (_) {
-        if (!arguments.length) {
-            return _rowOrdering;
-        }
-        _rowOrdering = _;
-        return _chart;
+        var rowValues = _chart.data().map(_chart.valueAccessor());
+        rowValues.sort(d3.ascending);
+        return d3.scale.ordinal().domain(rowValues.filter(uniq));
     };
 
     /**
      #### .cols([keys])
      Gets or sets the keys used to create the columns of the heatmap, as an array. By default, all
-     the values will be fetched from the data using the key accessor.
+     the values will be fetched from the data using the key accessor, and they will be sorted in
+     ascending order.
      **/
     _chart.cols = function (_) {
-        if (!arguments.length) {
+        if (arguments.length) {
+            _cols = _;
+            return _chart;
+        }
+        if (_cols) {
             return _cols;
         }
-        _cols = _;
-        return _chart;
-    };
-
-    /**
-     #### .colOrdering([orderFunction])
-     Get or set an accessor to order the cols.  Default is ascending.
-     */
-    _chart.colOrdering = function (_) {
-        if (!arguments.length) {
-            return _colOrdering;
-        }
-        _colOrdering = _;
-        return _chart;
+        var colValues = _chart.data().map(_chart.keyAccessor());
+        colValues.sort(d3.ascending);
+        return d3.scale.ordinal().domain(colValues.filter(uniq));
     };
 
     _chart._doRender = function () {
@@ -8002,19 +8000,9 @@ dc.heatMap = function (parent, chartGroup) {
     };
 
     _chart._doRedraw = function () {
-        var data = _chart.data(),
-            rows = _chart.rows() || data.map(_chart.valueAccessor()),
-            cols = _chart.cols() || data.map(_chart.keyAccessor());
-        if (_rowOrdering) {
-            rows = rows.sort(_rowOrdering);
-        }
-        if (_colOrdering) {
-            cols = cols.sort(_colOrdering);
-        }
-        rows = _rowScale.domain(rows);
-        cols = _colScale.domain(cols);
-
-        var rowCount = rows.domain().length,
+        var rows = _chart.rows(),
+            cols = _chart.cols(),
+            rowCount = rows.domain().length,
             colCount = cols.domain().length,
             boxWidth = Math.floor(_chart.effectiveWidth() / colCount),
             boxHeight = Math.floor(_chart.effectiveHeight() / rowCount);
@@ -8752,3 +8740,5 @@ return dc;}
     }
 }
 )();
+
+//# sourceMappingURL=dc.js.map
