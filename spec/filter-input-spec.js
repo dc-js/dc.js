91ecc78dfda60b401d7e5f6a2c12804d6c2aa75f
xavier <xavier@tttp.eu>
2015-05-14 13:14:16 +0200

    Input filter
    Add a wordcloud
    
    Add a new input-filter graph
    
    delete cloud for now
    
    clean text, remove dead code
    
    add configuration option for throttle duration and filter
    
    work on tests+update based on input on the PR and ML
    
    remove mandatory group() that isn't used
    
    latest version

---
+++ spec/filter-input-spec.js
@@ -0,0 +1,105 @@
+describe('dc.inputFilter', function() {
+
+    var id, chart, data;
+    var dimension, group;
+    var countryDimension;
+
+    beforeEach(function() {
+        dateFixture = loadDateFixture();
+        data = crossfilter(dateFixture);
+        dimension = data.dimension(function(d) {
+            return d.countrycode.toLowerCase()+ " "+ d.state.toLowerCase();
+        });
+        group =  dimension.group().reduceSum(function(d) { return 1; });
+        countryDimension = data.dimension(function(d) {
+            return d.countrycode;
+        });
+
+        id = "input-filter";
+        appendChartID(id);
+        chart = dc.inputFilter("#" + id)
+            .dimension(dimension)
+            .group(group);
+        chart.render();
+    });
+
+    describe('creation', function() {
+        it('generates something', function() {
+            expect(chart).not.toBeNull();
+        });
+        it('registers', function() {
+            expect(dc.hasChart(chart)).toBeTruthy();
+        });
+        it('sets an input field', function() {
+            expect(chart.selectAll("input")[0].length).toEqual(1);
+        });
+        it("doesn't filter by default", function() {
+            expect(chart.dimension().top(1000).length).toEqual(100);
+        });
+    });
+
+    describe('default accessor functions', function() {
+      it('exists for html()', function() {
+        var html = chart.html();
+        expect(typeof html).toBe("function");
+      });
+      it('exists for normalize()', function() {
+        var normalize = chart.normalize();
+        expect(typeof normalize).toBe("function");
+      });
+      it('exists for filterFunction()', function() {
+        var filterFunction = chart.filterFunction();
+        expect(typeof filterFunction).toBe("function");
+      });
+      it('exists for throttleDuration()', function() {
+        var throttleDuration = chart.throttle();
+        expect(typeof throttleDuration).toBe("number");
+        expect(throttleDuration).toBe(200);
+      });
+    });
+
+    describe('change the html search field', function() {
+        it('generates something', function() {
+            expect(chart).not.toBeNull();
+        });
+        it('registers', function() {
+            expect(dc.hasChart(chart)).toBeTruthy();
+        });
+        it('sets an input field', function() {
+            expect(chart.selectAll("input")[0].length).toEqual(1);
+        });
+        it("doesn't filter by default", function() {
+            expect(chart.dimension().top(1000).length).toEqual(100);
+        });
+    });
+
+    describe('filter when typing', function() {
+        var mockTyping = function (q) {
+          i=d3.select("input");
+          i[0][0].value=q;
+          i.on("input").call(i.node(),i.datum());
+        };
+
+        beforeEach(function() {
+            chart.redraw();
+        });
+
+        it("has a mock function that sets the value", function() {
+          mockTyping("42");
+          expect(chart.selectAll("input")[0][0].value),toEqual("42");
+        });
+
+        it(' a letter that exists', function() {
+            mockTyping("C");
+            expect(chart.selectAll("input")[0].length).toEqual(1);
+        });
+
+    });
+
+
+    afterEach(function() {
+        dimension.filterAll();
+    });
+});
+
+
---
+++ src/filter-input.js
@@ -0,0 +1,109 @@
+/**
+## input Filter Widget
+Includes: [Base Mixin](#base-mixin)
+
+The input filter data widget is a simple widget designed to display an input field allowing to filter the data
+that matches the text typed. 
+
+As opposed to the other graphs, this doesn't display any result and doesn't update its display, it's just to input an filter other graphs
+
+Examples:
+
+#### dc.inputFilter(parent[, chartGroup])
+Create an input widget and attach it to the given parent element.
+
+Parameters:
+
+* parent : string | node | selection - any valid
+ [d3 single selector](https://github.com/mbostock/d3/wiki/Selections#selecting-elements) specifying
+ a dom block element such as a div; or a dom element or d3 selection.
+* chartGroup : string (optional) - name of the chart group this widget should be placed in.
+
+Returns:
+A newly created input widget instance
+#### .dimension(data) - **mandatory**
+
+```js
+var data=[{"firstname":"John","lastname":"Coltrane"}{"firstname":"Miles",lastname:"Davis"] 
+var ndx = crossfilter(data);
+var dimension = ndx.dimension(function(d) {
+  return d.lastname.toLowerCase() + " "+ d.firstname.toLowerCase();
+});
+
+dc.inputFilter('.search')
+    .dimension(dimension);
+    // you don't need the group() function
+```
+
+**/
+
+dc.inputFilter = function (parent, chartGroup) {
+    var _chart = dc.baseMixin({});
+    var _html = function() {return "<input class='form-control input-lg' placeholder='search'/>"};
+    var _normalize = function (s) { return s.toLowerCase()};
+    var _filterFunction = function(q) { 
+      _chart.dimension().filterFunction(function (d){return d.indexOf (q) !== -1;});
+    };
+    var _throttleDuration=200;
+    var _throttleTimer;
+
+    _chart.group(function (){throw "the group function on inputFilter should never be called, please report the issue"});
+
+    _chart._doRender = function () {
+      _chart.root().html(_html());
+
+      _chart.root().selectAll("input").on("input", function() {
+        _filterFunction(_normalize(this.value));
+        _throttle();
+      });
+      return _chart;
+    };
+
+    function _throttle() {
+      window.clearTimeout(_throttleTimer);
+      _throttleTimer = window.setTimeout(function() {
+          dc.redrawAll();
+      }, _throttleDuration);
+    }
+
+    _chart._doRedraw = function () {
+      return _chart;
+    };
+
+    _chart.html = function (s) {
+        if (!arguments.length) {
+            return _html;
+        }
+        if (typeof s == "string")
+          _html = function (){return s};
+        else
+          _html = s;
+        return _chart;
+    };
+
+    _chart.normalize = function (s) {
+        if (!arguments.length) {
+            return _normalize;
+        }
+        _normalize = s;
+        return _chart;
+    };
+
+    _chart.filterFunction = function (s) {
+        if (!arguments.length) {
+            return _filterFunction;
+        }
+        _filterFunction = s;
+        return _chart;
+    };
+
+    _chart.throttleDuration = function (s) {
+        if (!arguments.length) {
+            return _throttleDuration;
+        }
+        _throttleDuration = s;
+        return _chart;
+    };
+
+    return _chart.anchor(parent, chartGroup);
+};
