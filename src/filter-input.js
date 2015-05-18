a576d8caf618531d5a915d90d01a16df8368aa7a
xavier <xavier@tttp.eu>
2015-05-19 01:06:43 +0200

    clean text, remove dead code

--- src/filter-input.js
+++ src/filter-input.js
@@ -23,8 +23,8 @@ Returns:
 A newly created input widget instance
 #### .dimension(data) - **mandatory**
 
-#### .group(groupAll) - **mandatory**
-For the data count widget the only valid group is the group returned by `dimension.groupAll()`.
+#### .group(group) - **mandatory**
+For the widget, a reduceSum works ok
 
 ```js
 var data=[{"firstname":"John","lastname":"Coltrane"}{"fistname":"Miles",lastname:"Davis"] 
@@ -97,57 +97,3 @@ dc.inputFilter = function (parent, chartGroup) {
 
     return _chart.anchor(parent, chartGroup);
 };
-
-    /**
-d3.select("#nRadius").on("input", function() {
-  update(+this.value);
-});
-
-
-  // Returns a function, that, when invoked, will only be triggered at most once
-  // during a given window of time. Normally, the throttled function will run
-  // as much as it can, without ever going more than once per `wait` duration;
-  // but if you'd like to disable the execution on the leading edge, pass
-  // `{leading: false}`. To disable execution on the trailing edge, ditto.
-  _.throttle = function(func, wait, options) {
-    var context, args, result;
-    var timeout = null;
-    var previous = 0;
-    if (!options) options = {};
-    var later = function() {
-      previous = options.leading === false ? 0 : _.now();
-      timeout = null;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    };
-    return function() {
-      var now = _.now();
-      if (!previous && options.leading === false) previous = now;
-      var remaining = wait - (now - previous);
-      context = this;
-      args = arguments;
-      if (remaining <= 0 || remaining > wait) {
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
-        }
-        previous = now;
-        result = func.apply(context, args);
-        if (!timeout) context = args = null;
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
-      }
-      return result;
-    };
-  };
-
-
-var throttleTimer;
-function throttle() {
-  window.clearTimeout(throttleTimer);
-    throttleTimer = window.setTimeout(function() {
-          redraw();
-        }, 200);
-}
-
-*/
