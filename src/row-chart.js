ba8bbc0e1d5d203ec39a39f9f99a0ee83d4597a2
ruhley <timothy.ruhle@gmail.com>
2015-06-05 09:22:18 +1000

    row chart useRightYAxis and examples

--- src/row-chart.js
+++ src/row-chart.js
@@ -53,6 +53,8 @@ dc.rowChart = function (parent, chartGroup) {
 
     var _rowData;
 
+    var _useRightYAxis = false;
+
     _chart.rowsCap = _chart.cap;
 
     function calculateAxisScale() {
@@ -61,8 +63,13 @@ dc.rowChart = function (parent, chartGroup) {
             if (extent[0] > 0) {
                 extent[0] = 0;
             }
-            _x = d3.scale.linear().domain(extent)
-                .range([0, _chart.effectiveWidth()]);
+
+            var domain = d3.scale.linear().domain(extent);
+            if (_useRightYAxis) {
+                _x = domain.range([_chart.effectiveWidth(), 0]);
+            } else {
+                _x = domain.range([0, _chart.effectiveWidth()]);
+            }
         }
         _xAxis.scale(_x);
     }
@@ -384,6 +391,19 @@ dc.rowChart = function (parent, chartGroup) {
             return _titleLabelOffsetX;
         }
         _titleLabelOffsetX = o;
+        return _chart;
+    };
+
+    /**
+     #### .useRightYAxis()
+     Gets or sets whether the chart should be drawn with a right axis instead of a left axis.
+     **/
+
+    _chart.useRightYAxis = function (_) {
+        if (!arguments.length) {
+            return _useRightYAxis;
+        }
+        _useRightYAxis = _;
         return _chart;
     };
 
