5189c1305fe8c2c32995c24e818ec0d97c61337b
Gordon Woodhull <gordon@woodhull.com>
2015-06-07 23:15:03 -0400

    improve transition tests
    query parameters, modularize, etc.

--- web/transitions/transition-test.js
+++ web/transitions/transition-test.js
@@ -1,10 +1,27 @@
-(function() {
-    var inter;
+var transitionTest = (function() {
+
+    // http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    var querystring = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=', 2);
+            if (p.length == 1)
+                b[p[0]] = "";
+            else
+                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'));
+
+    var inter, duration = +querystring.duration, pause = +querystring.pause;
+    if(isNaN(duration)) duration = 3000;
+    if(isNaN(pause)) pause = 500;
     function stop() {
         window.clearInterval(inter);
     }
-    window.stop = stop;
-    function back_and_forth(T, f1, f2) {
+    function oscillate(f1, f2) {
         return function() {
             stop();
             var which = false;
@@ -16,8 +33,13 @@
                 else
                     f1();
                 dc.redrawAll();
-            }, T);
+            }, duration+pause);
         };
     }
-    window.back_and_forth = back_and_forth;
+    return {
+        duration: duration,
+        pause: pause,
+        stop: stop,
+        oscillate: oscillate
+    };
 })();
