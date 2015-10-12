468033879fac0017a9006f54cf5fa7c11ad77cfe
Matt Traynham <mrt6467@alum.uncw.edu>
2015-10-12 13:42:54 -0400

    Fixing my own commit with the keyAccessor problem

--- src/box-plot.js
+++ src/box-plot.js
@@ -158,7 +158,7 @@ dc.boxPlot = function (parent, chartGroup) {
             .attr('transform', boxTransform)
             .call(_box)
             .on('click', function (d) {
-                _chart.filter(d.key);
+                _chart.filter(_chart.keyAccessor()(d));
                 _chart.redrawGroup();
             });
     }
@@ -190,8 +190,10 @@ dc.boxPlot = function (parent, chartGroup) {
                 var extent = _chart.brush().extent();
                 var start = extent[0];
                 var end = extent[1];
+                var keyAccessor = _chart.keyAccessor();
                 _chart.g().selectAll('g.box').each(function (d) {
-                    if (d.key < start || d.key >= end) {
+                    var key = keyAccessor(d);
+                    if (key < start || key >= end) {
                         _chart.fadeDeselected(this);
                     } else {
                         _chart.highlightSelected(this);
@@ -206,7 +208,7 @@ dc.boxPlot = function (parent, chartGroup) {
     };
 
     _chart.isSelectedNode = function (d) {
-        return _chart.hasFilter(d.key);
+        return _chart.hasFilter(_chart.keyAccessor()(d));
     };
 
     _chart.yAxisMin = function () {
