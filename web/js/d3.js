f65a60521f09440c23b463ae3668242132d9c571
Gordon Woodhull <gordon@woodhull.com>
2015-12-04 13:27:05 -0500

    artifacts

--- web/js/d3.js
+++ web/js/d3.js
@@ -1,6 +1,6 @@
 !function() {
   var d3 = {
-    version: "3.5.10"
+    version: "3.5.8"
   };
   var d3_arraySlice = [].slice, d3_array = function(list) {
     return d3_arraySlice.call(list);
@@ -1232,7 +1232,7 @@
         function ended() {
           if (!position(parent, dragId)) return;
           dragSubject.on(move + dragName, null).on(end + dragName, null);
-          dragRestore(dragged);
+          dragRestore(dragged && d3.event.target === target);
           dispatch({
             type: "dragend"
           });
@@ -1470,7 +1470,7 @@
       }), center0 = null;
     }
     function mousedowned() {
-      var that = this, dispatch = event.of(that, arguments), dragged = 0, subject = d3.select(d3_window(that)).on(mousemove, moved).on(mouseup, ended), location0 = location(d3.mouse(that)), dragRestore = d3_event_dragSuppress(that);
+      var that = this, target = d3.event.target, dispatch = event.of(that, arguments), dragged = 0, subject = d3.select(d3_window(that)).on(mousemove, moved).on(mouseup, ended), location0 = location(d3.mouse(that)), dragRestore = d3_event_dragSuppress(that);
       d3_selection_interrupt.call(that);
       zoomstarted(dispatch);
       function moved() {
@@ -1480,7 +1480,7 @@
       }
       function ended() {
         subject.on(mousemove, null).on(mouseup, null);
-        dragRestore(dragged);
+        dragRestore(dragged && d3.event.target === target);
         zoomended(dispatch);
       }
     }
@@ -6367,7 +6367,7 @@
           alpha = x;
         } else {
           timer.c = null, timer.t = NaN, timer = null;
-          event.end({
+          event.start({
             type: "end",
             alpha: alpha = 0
           });
@@ -8927,14 +8927,6 @@
           delete lock[cancelId];
         }
       }
-      timer.c = tick;
-      d3_timer(function() {
-        if (timer.c && tick(elapsed || 1)) {
-          timer.c = null;
-          timer.t = NaN;
-        }
-        return 1;
-      }, 0, time);
       lock.active = id;
       transition.event && transition.event.start.call(node, node.__data__, i);
       tweens = [];
@@ -8945,6 +8937,14 @@
       });
       ease = transition.ease;
       duration = transition.duration;
+      timer.c = tick;
+      d3_timer(function() {
+        if (timer.c && tick(elapsed || 1)) {
+          timer.c = null;
+          timer.t = NaN;
+        }
+        return 1;
+      }, 0, time);
     }
     function tick(elapsed) {
       var t = elapsed / duration, e = ease(t), n = tweens.length;
@@ -9546,5 +9546,5 @@
   d3.xml = d3_xhrType(function(request) {
     return request.responseXML;
   });
-  if (typeof define === "function" && define.amd) this.d3 = d3, define(d3); else if (typeof module === "object" && module.exports) module.exports = d3; else this.d3 = d3;
+  if (typeof define === "function" && define.amd) define(this.d3 = d3); else if (typeof module === "object" && module.exports) module.exports = d3; else this.d3 = d3;
 }();
