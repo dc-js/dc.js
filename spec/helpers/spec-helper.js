ebaf8c970115868269be4f85f8789d878255ea0b
Gordon Woodhull <gordon@woodhull.com>
2015-12-22 19:05:04 -0500

    trim succeeding tests
    because saucelabs barfs on output > 64k
    
    adapted from
    https://github.com/axemclion/grunt-saucelabs/issues/109#issuecomment-90829547

--- spec/helpers/spec-helper.js
+++ spec/helpers/spec-helper.js
@@ -1,6 +1,4 @@
 /* global parseTranslate */
-// reporter for saucelabs
-jasmine.getEnv().addReporter(new jasmine.JSReporter2());
 beforeEach(function () {
     d3.select('body').append('div').attr('id', 'test-content');
     jasmine.clock().install();
