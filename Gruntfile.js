84e6be66cee6d90cbd074e0acf090d7902b74702
Gordon Woodhull <gordon@woodhull.com>
2016-02-12 18:04:34 -0500

    use jsdoc.conf.json
    for #1086

--- Gruntfile.js
+++ Gruntfile.js
@@ -220,7 +220,7 @@ module.exports = function (grunt) {
                 options: {
                     destination: 'web/docs/html',
                     template: 'node_modules/ink-docstrap/template',
-                    configure: 'node_modules/ink-docstrap/template/jsdoc.conf.json'
+                    configure: 'jsdoc.conf.json'
                 }
             }
         },
