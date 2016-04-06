c7ac576c9b9980b11abe62bf2db6947d481c6cae
katiRG <cnangini@gmail.com>
2016-04-06 16:11:09 +0200

    specified first two elements of d.key to pass to isFiltered()

--- dc.js
+++ dc.js
@@ -9046,7 +9046,8 @@ dc.scatterPlot = function (parent, chartGroup) {
             .attr('transform', _locator);
 
         symbols.each(function (d, i) {
-            _filtered[i] = !_chart.filter() || _chart.filter().isFiltered(d.key);
+            //_filtered[i] = !_chart.filter() || _chart.filter().isFiltered(d.key);
+            _filtered[i] = !_chart.filter() || _chart.filter().isFiltered([d.key[0], d.key[1]]);
         });
 
         dc.transition(symbols, _chart.transitionDuration())
