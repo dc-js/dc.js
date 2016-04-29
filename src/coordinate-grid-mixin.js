0e4522d474080a6b042479c1d2ca537d9ede660f
Gordon Woodhull <gordon@woodhull.com>
2016-04-29 14:19:17 -0400

    clarify focus&range charts should share dimension
    for #1112
    .. or at least have compatible dimension keys
    Thanks @xinggao531!

--- src/coordinate-grid-mixin.js
+++ src/coordinate-grid-mixin.js
@@ -128,8 +128,13 @@ dc.coordinateGridMixin = function (_chart) {
      * Get or set the range selection chart associated with this instance. Setting the range selection
      * chart using this function will automatically update its selection brush when the current chart
      * zooms in. In return the given range chart will also automatically attach this chart as its focus
-     * chart hence zoom in when range brush updates. See the [Nasdaq 100
-     * Index](http://dc-js.github.com/dc.js/) example for this effect in action.
+     * chart hence zoom in when range brush updates.
+     *
+     * Usually the range and focus charts will share a dimension. The range chart will set the zoom
+     * boundaries for the focus chart, so its dimension values must be compatible with the domain of
+     * the focus chart.
+     *
+     * See the [Nasdaq 100 Index](http://dc-js.github.com/dc.js/) example for this effect in action.
      * @method rangeChart
      * @memberof dc.coordinateGridMixin
      * @instance
