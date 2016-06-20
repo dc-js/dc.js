936f72e2bba8b783ce6bf7252cfee5a99d193296
Alexander Stillesjö <a.stillesjo@gmail.com>
2016-06-20 07:38:14 +0200

    Don't interpolate angles when updating label paths
    not data

--- src/pie-chart.js
+++ src/pie-chart.js
@@ -246,8 +246,9 @@ dc.pieChart = function (parent, chartGroup) {
         if (transition.attrTween) {
             transition
                 .attrTween('points', function (d) {
-                    this._current = this._current || d;
-                    var interpolate = d3.interpolate(this._current, d);
+                    var current = this._current || d;
+                    current = {startAngle: current.startAngle, endAngle: current.endAngle};
+                    var interpolate = d3.interpolate(current, d);
                     this._current = interpolate(0);
                     return function (t) {
                         var d2 = interpolate(t);
