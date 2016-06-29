dc-js/dc.js

commit 354b89010eae538877fd78aeefebd2865a061d2f

resize brush handles on chart redraw
for #971
also, transition the brush and optionalTransition helper
also enables brush for resize-right-axis for testing purposes

src/coordinate-grid-mixin.js
@@ -871,7 +871,7 @@ dc.coordinateGridMixin = function (_chart) {
                 .attr('class', 'brush')
                 .attr('transform', 'translate(' + _chart.margins().left + ',' + _chart.margins().top + ')')
                 .call(_brush.x(_chart.x()));
-            _chart.setBrushY(gBrush);
+            _chart.setBrushY(gBrush, false);
             _chart.setHandlePaths(gBrush);
 
             if (_chart.hasFilter()) {
@@ -884,8 +884,10 @@ dc.coordinateGridMixin = function (_chart) {
         gBrush.selectAll('.resize').append('path').attr('d', _chart.resizeHandlePath);
     };
 
-    _chart.setBrushY = function (gBrush) {
-        gBrush.selectAll('rect').attr('height', brushHeight());
+    _chart.setBrushY = function (gBrush, doTransition) {
+        var transition = dc.optionalTransition(doTransition, _chart.transitionDuration());
+        transition(gBrush.selectAll('.brush rect')).attr('height', brushHeight());
+        transition(gBrush.selectAll('.resize path')).attr('d', _chart.resizeHandlePath);
     };
 
     _chart.extendBrush = function () {
@@ -932,7 +934,7 @@ dc.coordinateGridMixin = function (_chart) {
 
             var gBrush = g.select('g.brush');
             gBrush.call(_chart.brush().x(_chart.x()));
-            _chart.setBrushY(gBrush);
+            _chart.setBrushY(gBrush, true);
         }
 
         _chart.fadeDeselectedArea();

dc.js
@@ -218,6 +218,20 @@ dc.transition = function (selections, duration, callback) {
     return s;
 };
 
+/* somewhat silly, but to avoid duplicating logic */
+dc.optionalTransition = function (enable, duration, callback) {
+    if (enable) {
+        return function (selection) {
+            return dc.transition(selection, duration, callback);
+        };
+    }
+    else {
+        return function (selection) {
+            return selection;
+        };
+    }
+};
+
 dc.units = {};
 
 /**
@@ -2854,7 +2868,7 @@ dc.coordinateGridMixin = function (_chart) {
                 .attr('class', 'brush')
                 .attr('transform', 'translate(' + _chart.margins().left + ',' + _chart.margins().top + ')')
                 .call(_brush.x(_chart.x()));
-            _chart.setBrushY(gBrush);
+            _chart.setBrushY(gBrush, false);
             _chart.setHandlePaths(gBrush);
 
             if (_chart.hasFilter()) {
@@ -2867,8 +2881,10 @@ dc.coordinateGridMixin = function (_chart) {
         gBrush.selectAll('.resize').append('path').attr('d', _chart.resizeHandlePath);
     };
 
-    _chart.setBrushY = function (gBrush) {
-        gBrush.selectAll('rect').attr('height', brushHeight());
+    _chart.setBrushY = function (gBrush, doTransition) {
+        var transition = dc.optionalTransition(doTransition, _chart.transitionDuration());
+        transition(gBrush.selectAll('.brush rect')).attr('height', brushHeight());
+        transition(gBrush.selectAll('.resize path')).attr('d', _chart.resizeHandlePath);
     };
 
     _chart.extendBrush = function () {
@@ -2915,7 +2931,7 @@ dc.coordinateGridMixin = function (_chart) {
 
             var gBrush = g.select('g.brush');
             gBrush.call(_chart.brush().x(_chart.x()));
-            _chart.setBrushY(gBrush);
+            _chart.setBrushY(gBrush, true);
         }
 
         _chart.fadeDeselectedArea();

src/core.js
@@ -197,6 +197,20 @@ dc.transition = function (selections, duration, callback) {
     return s;
 };
 
+/* somewhat silly, but to avoid duplicating logic */
+dc.optionalTransition = function (enable, duration, callback) {
+    if (enable) {
+        return function (selection) {
+            return dc.transition(selection, duration, callback);
+        };
+    }
+    else {
+        return function (selection) {
+            return selection;
+        };
+    }
+};
+
 dc.units = {};
 
 /**

web/js/dc.js
@@ -218,6 +218,20 @@ dc.transition = function (selections, duration, callback) {
     return s;
 };
 
+/* somewhat silly, but to avoid duplicating logic */
+dc.optionalTransition = function (enable, duration, callback) {
+    if (enable) {
+        return function (selection) {
+            return dc.transition(selection, duration, callback);
+        };
+    }
+    else {
+        return function (selection) {
+            return selection;
+        };
+    }
+};
+
 dc.units = {};
 
 /**
@@ -2854,7 +2868,7 @@ dc.coordinateGridMixin = function (_chart) {
                 .attr('class', 'brush')
                 .attr('transform', 'translate(' + _chart.margins().left + ',' + _chart.margins().top + ')')
                 .call(_brush.x(_chart.x()));
-            _chart.setBrushY(gBrush);
+            _chart.setBrushY(gBrush, false);
             _chart.setHandlePaths(gBrush);
 
             if (_chart.hasFilter()) {
@@ -2867,8 +2881,10 @@ dc.coordinateGridMixin = function (_chart) {
         gBrush.selectAll('.resize').append('path').attr('d', _chart.resizeHandlePath);
     };
 
-    _chart.setBrushY = function (gBrush) {
-        gBrush.selectAll('rect').attr('height', brushHeight());
+    _chart.setBrushY = function (gBrush, doTransition) {
+        var transition = dc.optionalTransition(doTransition, _chart.transitionDuration());
+        transition(gBrush.selectAll('.brush rect')).attr('height', brushHeight());
+        transition(gBrush.selectAll('.resize path')).attr('d', _chart.resizeHandlePath);
     };
 
     _chart.extendBrush = function () {
@@ -2915,7 +2931,7 @@ dc.coordinateGridMixin = function (_chart) {
 
             var gBrush = g.select('g.brush');
             gBrush.call(_chart.brush().x(_chart.x()));
-            _chart.setBrushY(gBrush);
+            _chart.setBrushY(gBrush, true);
         }
 
         _chart.fadeDeselectedArea();

web/resizing/resizing-right-axis.html
@@ -68,7 +68,7 @@
                 .elasticY(true)
                 .renderHorizontalGridLines(true)
                 .legend(dc.legend().x(90).y(10).itemHeight(13).gap(5))
-                .brushOn(false)
+                .brushOn(true)
                 .compose([
                     dc.lineChart(moveChart)
                             .group(indexAvgByMonthGroup, "Monthly Index Average")

commit eb37c8056ed874c83d125292ecca2fc97ecb0d0f

seemingly wrong brush selector
that often worked
thanks @paulbriton for tracking this down!
this seems to be a mistake introduced in 354b890
fixes #1134

src/coordinate-grid-mixin.js
@@ -1009,7 +1009,7 @@ dc.coordinateGridMixin = function (_chart) {
     };
 
     _chart.setBrushY = function (gBrush) {
-        gBrush.selectAll('.brush rect')
+        gBrush.selectAll('rect')
             .attr('height', brushHeight());
         gBrush.selectAll('.resize path')
             .attr('d', _chart.resizeHandlePath);