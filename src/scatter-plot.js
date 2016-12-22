e12e97b2b614fde0e2463ebedde65f61a1a07b58
Gordon Woodhull <gordon@woodhull.com>
2016-12-22 17:31:40 -0500

    move renderTitles closer to where it's used
    and rename it so it's not clashing with the option
    and perhaps more consistent with some imagined consistency
    
    for #1200

--- src/scatter-plot.js
+++ src/scatter-plot.js
@@ -78,7 +78,7 @@ dc.scatterPlot = function (parent, chartGroup) {
             .attr('fill', _chart.getColor)
             .attr('transform', _locator);
 
-        symbols.call(renderTitle, _chart.data());
+        symbols.call(renderTitles, _chart.data());
 
         symbols.each(function (d, i) {
             _filtered[i] = !_chart.filter() || _chart.filter().isFiltered([d.key[0], d.key[1]]);
@@ -109,6 +109,15 @@ dc.scatterPlot = function (parent, chartGroup) {
         dc.transition(symbols.exit(), _chart.transitionDuration(), _chart.transitionDelay())
             .attr('opacity', 0).remove();
     };
+
+    function renderTitles (symbol, d) {
+        if (_chart.renderTitle()) {
+            symbol.selectAll('title').remove();
+            symbol.append('title').text(function (d) {
+                return _chart.title()(d);
+            });
+        }
+    }
 
     /**
      * Get or set the existence accessor.  If a point exists, it is drawn with
@@ -259,15 +268,6 @@ dc.scatterPlot = function (parent, chartGroup) {
         _emptySize = emptySize;
         return _chart;
     };
-
-    function renderTitle (symbol, d) {
-        if (_chart.renderTitle()) {
-            symbol.selectAll('title').remove();
-            symbol.append('title').text(function (d) {
-                return _chart.title()(d);
-            });
-        }
-    }
 
     /**
      * Set or get color for symbols when the group is empty. If null, just use the
