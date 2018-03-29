996a1813efb9874eff6d21c37a392baa02b22e5d
deepak <deepak@kreatio.com>
2018-03-29 12:20:52 +0530

    Code cleanup, lint clean

--- spec/filter-input-spec.js
+++ spec/filter-input-spec.js
@@ -1,105 +1,105 @@
-describe('dc.inputFilter', function() {
-
+/* global loadDateFixture, appendChartID */
+describe('dc.inputFilter', function () {
+    var dateFixture;
     var id, chart, data;
     var dimension, group;
     var countryDimension;
 
-    beforeEach(function() {
+    beforeEach(function () {
         dateFixture = loadDateFixture();
         data = crossfilter(dateFixture);
-        dimension = data.dimension(function(d) {
-            return d.countrycode.toLowerCase()+ " "+ d.state.toLowerCase();
+        dimension = data.dimension(function (d) {
+            return d.countrycode.toLowerCase() + ' ' + d.state.toLowerCase();
         });
-        group =  dimension.group().reduceSum(function(d) { return 1; });
-        countryDimension = data.dimension(function(d) {
+        group = dimension.group().reduceSum(function (d) {
+            return 1;
+        });
+        countryDimension = data.dimension(function (d) {
             return d.countrycode;
         });
 
-        id = "input-filter";
+        id = 'input-filter';
         appendChartID(id);
-        chart = dc.inputFilter("#" + id)
+        chart = dc.inputFilter('#' + id)
             .dimension(dimension)
             .group(group);
         chart.render();
     });
 
-    describe('creation', function() {
-        it('generates something', function() {
+    describe('creation', function () {
+        it('generates something', function () {
             expect(chart).not.toBeNull();
         });
-        it('registers', function() {
+        it('registers', function () {
             expect(dc.hasChart(chart)).toBeTruthy();
         });
-        it('sets an input field', function() {
-            expect(chart.selectAll("input")[0].length).toEqual(1);
+        it('sets an input field', function () {
+            expect(chart.selectAll('input').nodes().length).toEqual(1);
         });
-        it("doesn't filter by default", function() {
+        it('doesn\'t filter by default', function () {
             expect(chart.dimension().top(1000).length).toEqual(100);
         });
     });
 
-    describe('default accessor functions', function() {
-      it('exists for html()', function() {
-        var html = chart.html();
-        expect(typeof html).toBe("function");
-      });
-      it('exists for normalize()', function() {
-        var normalize = chart.normalize();
-        expect(typeof normalize).toBe("function");
-      });
-      it('exists for filterFunction()', function() {
-        var filterFunction = chart.filterFunction();
-        expect(typeof filterFunction).toBe("function");
-      });
-      it('exists for throttleDuration()', function() {
-        var throttleDuration = chart.throttle();
-        expect(typeof throttleDuration).toBe("number");
-        expect(throttleDuration).toBe(200);
-      });
+    describe('default accessor functions', function () {
+        it('exists for html()', function () {
+            var html = chart.html();
+            expect(typeof html).toBe('function');
+        });
+        it('exists for normalize()', function () {
+            var normalize = chart.normalize();
+            expect(typeof normalize).toBe('function');
+        });
+        it('exists for filterFunction()', function () {
+            var filterFunction = chart.filterFunction();
+            expect(typeof filterFunction).toBe('function');
+        });
+        it('exists for throttleDuration()', function () {
+            var throttleDuration = chart.throttleDuration();
+            expect(typeof throttleDuration).toBe('number');
+            expect(throttleDuration).toBe(200);
+        });
     });
 
-    describe('change the html search field', function() {
-        it('generates something', function() {
+    describe('change the html search field', function () {
+        it('generates something', function () {
             expect(chart).not.toBeNull();
         });
-        it('registers', function() {
+        it('registers', function () {
             expect(dc.hasChart(chart)).toBeTruthy();
         });
-        it('sets an input field', function() {
-            expect(chart.selectAll("input")[0].length).toEqual(1);
+        it('sets an input field', function () {
+            expect(chart.selectAll('input').nodes().length).toEqual(1);
         });
-        it("doesn't filter by default", function() {
+        it('doesn\'t filter by default', function () {
             expect(chart.dimension().top(1000).length).toEqual(100);
         });
     });
 
-    describe('filter when typing', function() {
+    describe('filter when typing', function () {
         var mockTyping = function (q) {
-          i=d3.select("input");
-          i[0][0].value=q;
-          i.on("input").call(i.node(),i.datum());
+            var i = d3.select('input');
+            i.nodes()[0].value = q;
+            i.on('input').call(i.node(), i.datum());
         };
 
-        beforeEach(function() {
+        beforeEach(function () {
             chart.redraw();
         });
 
-        it("has a mock function that sets the value", function() {
-          mockTyping("42");
-          expect(chart.selectAll("input")[0][0].value),toEqual("42");
+        it('has a mock function that sets the value', function () {
+            mockTyping('42');
+            expect(chart.selectAll('input').nodes()[0].value).toEqual('42');
         });
 
-        it(' a letter that exists', function() {
-            mockTyping("C");
-            expect(chart.selectAll("input")[0].length).toEqual(1);
+        it('a letter that exists', function () {
+            mockTyping('C');
+            expect(chart.selectAll('input').nodes().length).toEqual(1);
         });
 
     });
 
-
-    afterEach(function() {
+    afterEach(function () {
         dimension.filterAll();
     });
 });
-
-
