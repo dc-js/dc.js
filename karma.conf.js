251fd231ace416b03648a7ed0567b362fca0e081
deepak <deepak@kreatio.com>
2018-03-31 08:31:13 +0530

    Initial Karma support

---
+++ karma.conf.js
@@ -0,0 +1,77 @@
+// Karma configuration
+// Generated on Fri Mar 30 2018 23:33:52 GMT+0530 (IST)
+
+module.exports = function (config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            // Helpers
+            'web/js/jasmine-jsreporter.js',
+            'spec/helpers/*.js',
+            // JS code dependencies
+            'web/js/d3.js',
+            'web/js/crossfilter.js',
+            'web/js/colorbrewer.js',
+            // Code to be tested
+            'web/js/dc.js',
+            // Jasmine spec files
+            'spec/*spec.js'
+        ],
+
+
+        // list of files / patterns to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['dots'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Firefox', 'Chrome'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        // Concurrency level
+        // how many browser should be started simultaneously
+        concurrency: Infinity
+    })
+};
