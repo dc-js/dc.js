2a78edacf810b47e0f632525de3617a276b42e4d
deepak <deepak@kreatio.com>
2018-03-31 21:09:38 +0530

    Concurrency at Travis to 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -286,7 +286,7 @@ module.exports = function (grunt) {
             unit: {},
             ci: {
                 browsers: ['Chrome', 'Firefox'],
-                concurrency: 2,
+                concurrency: 1,
                 reporters: ['dots', 'summary']
             },
             sauceLabs: {
