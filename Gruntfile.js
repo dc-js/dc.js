a0f155d93c479e4d2c84cd234e42d21f07fa031c
deepak <deepak@kreatio.com>
2018-04-01 22:32:13 +0530

    D3v5 compatibility

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,7 +292,7 @@ module.exports = function (grunt) {
                             '<%= conf.pkg.name %>.js.map',
                             '<%= conf.pkg.name %>.min.js',
                             '<%= conf.pkg.name %>.min.js.map',
-                            'node_modules/d3/build/d3.js',
+                            'node_modules/d3/dist/d3.js',
                             'node_modules/crossfilter2/crossfilter.js',
                             'node_modules/queue-async/build/queue.js',
                             'node_modules/file-saver/FileSaver.js'
