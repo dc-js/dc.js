30209ba91036254f91278f7d4ac711d09207b270
deepak <deepak@kreatio.com>
2018-04-30 19:03:39 +0530

    More defencive script

--- scripts/adjust-for-d3v4.sh
+++ scripts/adjust-for-d3v4.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 npm i 'd3@^4'
 
 sed -i -e 's#node_modules/d3/dist/d3\.js#node_modules/d3/build/d3.js#' Gruntfile.js
 
 sed -i -e 's#// changedInD3v51#changedInD3v51#' spec/color-spec.js
+
+npm list d3
