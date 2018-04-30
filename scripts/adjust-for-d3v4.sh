bfb094e9f5b618de8a4dba2d8c0f55b70499c089
deepak <deepak@kreatio.com>
2018-04-30 19:47:45 +0530

    Make it OSX compliant, `sed -i` works differently

--- scripts/adjust-for-d3v4.sh
+++ scripts/adjust-for-d3v4.sh
@@ -9,10 +9,10 @@ set -x
 npm i 'd3@^4'
 
 # Path of d3.js is different in d3v4
-sed -i -e 's#node_modules/d3/dist/d3\.js#node_modules/d3/build/d3.js#' Gruntfile.js
+sed -i.orig -e 's#node_modules/d3/dist/d3\.js#node_modules/d3/build/d3.js#' Gruntfile.js
 
 # One of the test case varies for d3 lower than v5.1
-sed -i -e 's#// changedInD3v51#changedInD3v51#' spec/color-spec.js
+sed -i.orig -e 's#// changedInD3v51#changedInD3v51#' spec/color-spec.js
 
 # Display the d3 version
 npm list d3
