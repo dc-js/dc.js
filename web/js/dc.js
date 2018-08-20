b742a58b16493e55b4e27601ca5ff7faf7ea7506
girlwithglasses <amelia.ireland@gmail.com>
2018-08-20 07:04:36 +0100

    Updating to master

--- web/js/dc.js
+++ web/js/dc.js
@@ -539,7 +539,9 @@ dc.printers.filter = function (filter) {
     if (typeof filter !== 'undefined' && filter !== null) {
         if (filter instanceof Array) {
             if (filter.length >= 2) {
-                s = '[' + dc.utils.printSingleValue(filter[0]) + ' -> ' + dc.utils.printSingleValue(filter[1]) + ']';
+                s = '[' + filter.map( function(e) {
+                  return dc.utils.printSingleValue(e);
+                }).join(' -> ') + ']';
             } else if (filter.length >= 1) {
                 s = dc.utils.printSingleValue(filter[0]);
             }
