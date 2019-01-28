d14ca9e852426203cfbf7b47634ea69152b0a974
Gordon Woodhull <gordon@woodhull.com>
2019-01-28 14:33:22 -0500

    polyfill Element.matches for recent IE
    some indication this might not work on older IE, but this is getting ridiculous

--- src/core.js
+++ src/core.js
@@ -439,3 +439,23 @@ dc.renderlet = function (_) {
 dc.instanceOfChart = function (o) {
     return o instanceof Object && o.__dcFlag__ && true;
 };
+
+// polyfill for IE
+// from https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function (s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i = matches.length;
+            do {
+                --i;
+            }
+            while (i >= 0 && matches.item(i) !== this);
+            return i > -1;
+        };
+}
