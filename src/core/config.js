4300121bb3fc66c0784596f1a2ab6ab155609905
Deepak Kumar <deepak@kreatio.com>
2019-08-13 17:16:44 +0530

    lint clean :)

--- src/core/config.js
+++ src/core/config.js
@@ -1,4 +1,4 @@
-import { logger } from './logger';
+import {logger} from './logger';
 import * as d3 from 'd3';
 
 /**
@@ -60,10 +60,10 @@ export class Config {
 
 // D3v5 has removed schemeCategory20c, copied here for backward compatibility
 Config._schemeCategory20c = [
-        '#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d',
-        '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476',
-        '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc',
-        '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9'];
+    '#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d',
+    '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354', '#74c476',
+    '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc',
+    '#dadaeb', '#636363', '#969696', '#bdbdbd', '#d9d9d9'];
 
 /**
  * General configuration
