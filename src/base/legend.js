ffdd181bb7f62a800a23e43b4c2c7ceb78be84d9
Deepak Kumar <deepak@kreatio.com>
2019-08-18 07:57:22 +0530

    Making methods member functions

--- src/base/legend.js
+++ src/base/legend.js
@@ -16,89 +16,83 @@ import {pluck, utils} from '../core/utils';
 export class Legend {
     constructor () {
         const LABEL_GAP = 2;
+        
+        let self = this;
 
-        let _parent;
-        let _x = 0;
-        let _y = 0;
-        let _itemHeight = 12;
-        let _gap = 5;
-        let _horizontal = false;
-        let _legendWidth = 560;
-        let _itemWidth = 70;
-        let _autoItemWidth = false;
-        let _legendText = pluck('name');
-        let _maxItems;
+        self._parent = undefined;
+        self._x = 0;
+        self._y = 0;
+        self._itemHeight = 12;
+        self._gap = 5;
+        self._horizontal = false;
+        self._legendWidth = 560;
+        self._itemWidth = 70;
+        self._autoItemWidth = false;
+        self._legendText = pluck('name');
+        self._maxItems = undefined;
 
-        let _g;
-
-        this.parent = function (p) {
-            if (!arguments.length) {
-                return _parent;
-            }
-            _parent = p;
-            return this;
-        };
+        self._g;
 
         this.render = () => {
-            _parent.svg().select('g.dc-legend').remove();
-            _g = _parent.svg().append('g')
+            self._parent.svg().select('g.dc-legend').remove();
+            self._g = self._parent.svg().append('g')
                 .attr('class', 'dc-legend')
-                .attr('transform', 'translate(' + _x + ',' + _y + ')');
-            let legendables = _parent.legendables();
+                .attr('transform', 'translate(' + self._x + ',' + self._y + ')');
+            let legendables = self._parent.legendables();
 
-            if (_maxItems !== undefined) {
-                legendables = legendables.slice(0, _maxItems);
+            if (self._maxItems !== undefined) {
+                legendables = legendables.slice(0, self._maxItems);
             }
 
-            const itemEnter = _g.selectAll('g.dc-legend-item')
+            const itemEnter = self._g.selectAll('g.dc-legend-item')
                 .data(legendables)
                 .enter()
                 .append('g')
                 .attr('class', 'dc-legend-item')
                 .on('mouseover', d => {
-                    _parent.legendHighlight(d);
+                    self._parent.legendHighlight(d);
                 })
                 .on('mouseout', d => {
-                    _parent.legendReset(d);
+                    self._parent.legendReset(d);
                 })
                 .on('click', d => {
                     d.chart.legendToggle(d);
                 });
 
-            _g.selectAll('g.dc-legend-item')
+            self._g.selectAll('g.dc-legend-item')
                 .classed('fadeout', d => d.chart.isLegendableHidden(d));
 
             if (legendables.some(pluck('dashstyle'))) {
                 itemEnter
                     .append('line')
                     .attr('x1', 0)
-                    .attr('y1', _itemHeight / 2)
-                    .attr('x2', _itemHeight)
-                    .attr('y2', _itemHeight / 2)
+                    .attr('y1', self._itemHeight / 2)
+                    .attr('x2', self._itemHeight)
+                    .attr('y2', self._itemHeight / 2)
                     .attr('stroke-width', 2)
                     .attr('stroke-dasharray', pluck('dashstyle'))
                     .attr('stroke', pluck('color'));
             } else {
                 itemEnter
                     .append('rect')
-                    .attr('width', _itemHeight)
-                    .attr('height', _itemHeight)
+                    .attr('width', self._itemHeight)
+                    .attr('height', self._itemHeight)
                     .attr('fill', d => d ? d.color : 'blue');
             }
 
             itemEnter.append('text')
-                .text(_legendText)
-                .attr('x', _itemHeight + LABEL_GAP)
+                .text(self._legendText)
+                .attr('x', self._itemHeight + LABEL_GAP)
                 .attr('y', function () {
-                    return _itemHeight / 2 + (this.clientHeight ? this.clientHeight : 13) / 2 - 2;
+                    return self._itemHeight / 2 + (this.clientHeight ? this.clientHeight : 13) / 2 - 2;
                 });
 
             let cumulativeLegendTextWidth = 0;
             let row = 0;
             itemEnter.attr('transform', function (d, i) {
-                if (_horizontal) {
-                    const itemWidth = _autoItemWidth === true ? this.getBBox().width + _gap : _itemWidth;
-                    if ((cumulativeLegendTextWidth + itemWidth) > _legendWidth && cumulativeLegendTextWidth > 0) {
+                if (self._horizontal) {
+                    const itemWidth = self._autoItemWidth === true ? this.getBBox().width + self._gap : self._itemWidth;
+                    if ((cumulativeLegendTextWidth + itemWidth) > self._legendWidth && cumulativeLegendTextWidth > 0) {
                         ++row;
                         cumulativeLegendTextWidth = 0;
                     }
@@ -112,183 +106,190 @@ export class Legend {
         };
 
         function legendItemHeight () {
-            return _gap + _itemHeight;
+            return self._gap + self._itemHeight;
         }
-
-        /**
-         * Set or get x coordinate for legend widget.
-         * @method x
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [x=0]
-         * @returns {Number|dc.legend}
-         */
-        this.x = function (x) {
-            if (!arguments.length) {
-                return _x;
-            }
-            _x = x;
-            return this;
-        };
-
-        /**
-         * Set or get y coordinate for legend widget.
-         * @method y
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [y=0]
-         * @returns {Number|dc.legend}
-         */
-        this.y = function (y) {
-            if (!arguments.length) {
-                return _y;
-            }
-            _y = y;
-            return this;
-        };
-
-        /**
-         * Set or get gap between legend items.
-         * @method gap
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [gap=5]
-         * @returns {Number|dc.legend}
-         */
-        this.gap = function (gap) {
-            if (!arguments.length) {
-                return _gap;
-            }
-            _gap = gap;
-            return this;
-        };
-
-        /**
-         * Set or get legend item height.
-         * @method itemHeight
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [itemHeight=12]
-         * @returns {Number|dc.legend}
-         */
-        this.itemHeight = function (itemHeight) {
-            if (!arguments.length) {
-                return _itemHeight;
-            }
-            _itemHeight = itemHeight;
-            return this;
-        };
-
-        /**
-         * Position legend horizontally instead of vertically.
-         * @method horizontal
-         * @memberof dc.legend
-         * @instance
-         * @param  {Boolean} [horizontal=false]
-         * @returns {Boolean|dc.legend}
-         */
-        this.horizontal = function (horizontal) {
-            if (!arguments.length) {
-                return _horizontal;
-            }
-            _horizontal = horizontal;
-            return this;
-        };
-
-        /**
-         * Maximum width for horizontal legend.
-         * @method legendWidth
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [legendWidth=500]
-         * @returns {Number|dc.legend}
-         */
-        this.legendWidth = function (legendWidth) {
-            if (!arguments.length) {
-                return _legendWidth;
-            }
-            _legendWidth = legendWidth;
-            return this;
-        };
-
-        /**
-         * Legend item width for horizontal legend.
-         * @method itemWidth
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [itemWidth=70]
-         * @returns {Number|dc.legend}
-         */
-        this.itemWidth = function (itemWidth) {
-            if (!arguments.length) {
-                return _itemWidth;
-            }
-            _itemWidth = itemWidth;
-            return this;
-        };
-
-        /**
-         * Turn automatic width for legend items on or off. If true, {@link dc.legend#itemWidth itemWidth} is ignored.
-         * This setting takes into account the {@link dc.legend#gap gap}.
-         * @method autoItemWidth
-         * @memberof dc.legend
-         * @instance
-         * @param  {Boolean} [autoItemWidth=false]
-         * @returns {Boolean|dc.legend}
-         */
-        this.autoItemWidth = function (autoItemWidth) {
-            if (!arguments.length) {
-                return _autoItemWidth;
-            }
-            _autoItemWidth = autoItemWidth;
-            return this;
-        };
-
-        /**
-         * Set or get the legend text function. The legend widget uses this function to render the legend
-         * text for each item. If no function is specified the legend widget will display the names
-         * associated with each group.
-         * @method legendText
-         * @memberof dc.legend
-         * @instance
-         * @param  {Function} [legendText]
-         * @returns {Function|dc.legend}
-         * @example
-         * // default legendText
-         * legend.legendText(dc.pluck('name'))
-         *
-         * // create numbered legend items
-         * chart.legend(dc.legend().legendText(function(d, i) { return i + '. ' + d.name; }))
-         *
-         * // create legend displaying group counts
-         * chart.legend(dc.legend().legendText(function(d) { return d.name + ': ' d.data; }))
-         **/
-        this.legendText = function (legendText) {
-            if (!arguments.length) {
-                return _legendText;
-            }
-            _legendText = legendText;
-            return this;
-        };
-
-        /**
-         * Maximum number of legend items to display
-         * @method maxItems
-         * @memberof dc.legend
-         * @instance
-         * @param  {Number} [maxItems]
-         * @return {dc.legend}
-         */
-        this.maxItems = function (maxItems) {
-            if (!arguments.length) {
-                return _maxItems;
-            }
-            _maxItems = utils.isNumber(maxItems) ? maxItems : undefined;
-            return this;
-        };
-
-        return this;
     }
+
+    parent (p) {
+        if (!arguments.length) {
+            return this._parent;
+        }
+        this._parent = p;
+        return this;
+    };
+
+    /**
+     * Set or get x coordinate for legend widget.
+     * @method x
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [x=0]
+     * @returns {Number|dc.legend}
+     */
+    x (x) {
+        if (!arguments.length) {
+            return this._x;
+        }
+        this._x = x;
+        return this;
+    };
+
+    /**
+     * Set or get y coordinate for legend widget.
+     * @method y
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [y=0]
+     * @returns {Number|dc.legend}
+     */
+    y (y) {
+        if (!arguments.length) {
+            return this._y;
+        }
+        this._y = y;
+        return this;
+    };
+
+    /**
+     * Set or get gap between legend items.
+     * @method gap
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [gap=5]
+     * @returns {Number|dc.legend}
+     */
+    gap (gap) {
+        if (!arguments.length) {
+            return this._gap;
+        }
+        this._gap = gap;
+        return this;
+    };
+
+    /**
+     * Set or get legend item height.
+     * @method itemHeight
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [itemHeight=12]
+     * @returns {Number|dc.legend}
+     */
+    itemHeight (itemHeight) {
+        if (!arguments.length) {
+            return this._itemHeight;
+        }
+        this._itemHeight = itemHeight;
+        return this;
+    };
+
+    /**
+     * Position legend horizontally instead of vertically.
+     * @method horizontal
+     * @memberof dc.legend
+     * @instance
+     * @param  {Boolean} [horizontal=false]
+     * @returns {Boolean|dc.legend}
+     */
+    horizontal (horizontal) {
+        if (!arguments.length) {
+            return this._horizontal;
+        }
+        this._horizontal = horizontal;
+        return this;
+    };
+
+    /**
+     * Maximum width for horizontal legend.
+     * @method legendWidth
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [legendWidth=500]
+     * @returns {Number|dc.legend}
+     */
+    legendWidth (legendWidth) {
+        if (!arguments.length) {
+            return this._legendWidth;
+        }
+        this._legendWidth = legendWidth;
+        return this;
+    };
+
+    /**
+     * Legend item width for horizontal legend.
+     * @method itemWidth
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [itemWidth=70]
+     * @returns {Number|dc.legend}
+     */
+    itemWidth (itemWidth) {
+        if (!arguments.length) {
+            return this._itemWidth;
+        }
+        this._itemWidth = itemWidth;
+        return this;
+    };
+
+    /**
+     * Turn automatic width for legend items on or off. If true, {@link dc.legend#itemWidth itemWidth} is ignored.
+     * This setting takes into account the {@link dc.legend#gap gap}.
+     * @method autoItemWidth
+     * @memberof dc.legend
+     * @instance
+     * @param  {Boolean} [autoItemWidth=false]
+     * @returns {Boolean|dc.legend}
+     */
+    autoItemWidth (autoItemWidth) {
+        if (!arguments.length) {
+            return this._autoItemWidth;
+        }
+        this._autoItemWidth = autoItemWidth;
+        return this;
+    };
+
+    /**
+     * Set or get the legend text function. The legend widget uses this function to render the legend
+     * text for each item. If no function is specified the legend widget will display the names
+     * associated with each group.
+     * @method legendText
+     * @memberof dc.legend
+     * @instance
+     * @param  {Function} [legendText]
+     * @returns {Function|dc.legend}
+     * @example
+     * // default legendText
+     * legend.legendText(dc.pluck('name'))
+     *
+     * // create numbered legend items
+     * chart.legend(dc.legend().legendText(function(d, i) { return i + '. ' + d.name; }))
+     *
+     * // create legend displaying group counts
+     * chart.legend(dc.legend().legendText(function(d) { return d.name + ': ' d.data; }))
+     **/
+    legendText (legendText) {
+        if (!arguments.length) {
+            return this._legendText;
+        }
+        this._legendText = legendText;
+        return this;
+    };
+
+    /**
+     * Maximum number of legend items to display
+     * @method maxItems
+     * @memberof dc.legend
+     * @instance
+     * @param  {Number} [maxItems]
+     * @return {dc.legend}
+     */
+    maxItems (maxItems) {
+        if (!arguments.length) {
+            return this._maxItems;
+        }
+        this._maxItems = utils.isNumber(maxItems) ? maxItems : undefined;
+        return this;
+    };
+
 }
 
 export const legend = () => new Legend();
