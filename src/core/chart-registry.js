4e2390daf39c725f5303e97c46ceb9249f7213d7
Deepak Kumar <deepak@kreatio.com>
2019-12-01 16:06:10 +0530

    Convert ChartRegistry to ES6 class.

--- src/core/chart-registry.js
+++ src/core/chart-registry.js
@@ -1,6 +1,93 @@
 import {constants} from './constants';
 import {config} from './config';
 
+class ChartRegistry {
+    constructor () {
+        // chartGroup:string => charts:array
+        this._chartMap = {};
+    }
+
+    _initializeChartGroup (group) {
+        if (!group) {
+            group = constants.DEFAULT_CHART_GROUP;
+        }
+
+        if (!(this._chartMap)[group]) {
+            (this._chartMap)[group] = [];
+        }
+
+        return group;
+    }
+
+    /**
+     * Determine if a given chart instance resides in any group in the registry.
+     * @param {Object} chart dc.js chart instance
+     * @returns {Boolean}
+     */
+    has (chart) {
+        for (const e in this._chartMap) {
+            if ((this._chartMap)[e].indexOf(chart) >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Add given chart instance to the given group, creating the group if necessary.
+     * If no group is provided, the default group `constants.DEFAULT_CHART_GROUP` will be used.
+     * @param {Object} chart dc.js chart instance
+     * @param {String} [group] Group name
+     * @return {undefined}
+     */
+    register (chart, group) {
+        const _chartMap = this._chartMap;
+        group = this._initializeChartGroup(group);
+        _chartMap[group].push(chart);
+    }
+
+    /**
+     * Remove given chart instance from the given group, creating the group if necessary.
+     * If no group is provided, the default group `constants.DEFAULT_CHART_GROUP` will be used.
+     * @param {Object} chart dc.js chart instance
+     * @param {String} [group] Group name
+     * @return {undefined}
+     */
+    deregister (chart, group) {
+        group = this._initializeChartGroup(group);
+        for (let i = 0; i < (this._chartMap)[group].length; i++) {
+            if ((this._chartMap)[group][i].anchorName() === chart.anchorName()) {
+                (this._chartMap)[group].splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Clear given group if one is provided, otherwise clears all groups.
+     * @param {String} group Group name
+     * @return {undefined}
+     */
+    clear (group) {
+        if (group) {
+            delete (this._chartMap)[group];
+        } else {
+            this._chartMap = {};
+        }
+    }
+
+    /**
+     * Get an array of each chart instance in the given group.
+     * If no group is provided, the charts in the default group are returned.
+     * @param {String} [group] Group name
+     * @returns {Array<Object>}
+     */
+    list (group) {
+        group = this._initializeChartGroup(group);
+        return (this._chartMap)[group];
+    }
+}
+
 /**
  * The chartRegistry object maintains sets of all instantiated dc.js charts under named groups
  * and the default group.
@@ -11,105 +98,8 @@ import {config} from './config';
  * {@link renderAll renderAll}, {@link redrawAll redrawAll}, or chart functions
  * {@link baseMixin#renderGroup baseMixin.renderGroup},
  * {@link baseMixin#redrawGroup baseMixin.redrawGroup} are called.
- *
- * @namespace chartRegistry
- * @type {{has, register, deregister, clear, list}}
  */
-export const chartRegistry = (function () {
-    // chartGroup:string => charts:array
-    let _chartMap = {};
-
-    function initializeChartGroup (group) {
-        if (!group) {
-            group = constants.DEFAULT_CHART_GROUP;
-        }
-
-        if (!_chartMap[group]) {
-            _chartMap[group] = [];
-        }
-
-        return group;
-    }
-
-    return {
-        /**
-         * Determine if a given chart instance resides in any group in the registry.
-         * @method has
-         * @memberof chartRegistry
-         * @param {Object} chart dc.js chart instance
-         * @returns {Boolean}
-         */
-        has: function (chart) {
-            for (const e in _chartMap) {
-                if (_chartMap[e].indexOf(chart) >= 0) {
-                    return true;
-                }
-            }
-            return false;
-        },
-
-        /**
-         * Add given chart instance to the given group, creating the group if necessary.
-         * If no group is provided, the default group `constants.DEFAULT_CHART_GROUP` will be used.
-         * @method register
-         * @memberof chartRegistry
-         * @param {Object} chart dc.js chart instance
-         * @param {String} [group] Group name
-         * @return {undefined}
-         */
-        register: function (chart, group) {
-            group = initializeChartGroup(group);
-            _chartMap[group].push(chart);
-        },
-
-        /**
-         * Remove given chart instance from the given group, creating the group if necessary.
-         * If no group is provided, the default group `constants.DEFAULT_CHART_GROUP` will be used.
-         * @method deregister
-         * @memberof chartRegistry
-         * @param {Object} chart dc.js chart instance
-         * @param {String} [group] Group name
-         * @return {undefined}
-         */
-        deregister: function (chart, group) {
-            group = initializeChartGroup(group);
-            for (let i = 0; i < _chartMap[group].length; i++) {
-                if (_chartMap[group][i].anchorName() === chart.anchorName()) {
-                    _chartMap[group].splice(i, 1);
-                    break;
-                }
-            }
-        },
-
-        /**
-         * Clear given group if one is provided, otherwise clears all groups.
-         * @method clear
-         * @memberof chartRegistry
-         * @param {String} group Group name
-         * @return {undefined}
-         */
-        clear: function (group) {
-            if (group) {
-                delete _chartMap[group];
-            } else {
-                _chartMap = {};
-            }
-        },
-
-        /**
-         * Get an array of each chart instance in the given group.
-         * If no group is provided, the charts in the default group are returned.
-         * @method list
-         * @memberof chartRegistry
-         * @param {String} [group] Group name
-         * @returns {Array<Object>}
-         */
-        list: function (group) {
-            group = initializeChartGroup(group);
-            return _chartMap[group];
-        }
-    };
-})();
+export const chartRegistry = new ChartRegistry();
 
 /**
  * Add given chart instance to the given group, creating the group if necessary.
