28d434239a31c61ff4d62f783b3d0c97c9e507ba
Gordon Woodhull <gordon@woodhull.com>
2020-04-27 19:21:08 -0400

    document the apply_resizing and no-transitions functions

--- web-src/resizing/dc-restore-transition-duration.js
+++ web-src/resizing/dc-restore-transition-duration.js
@@ -1,3 +1,9 @@
+// for a getter/setter property name and a value,
+// return a function that wraps a function so that the property
+// is set to the new value, the wrapped function is run on the chart,
+// and the property value is then restored
+
+// also applies to arrays of charts, and children of composite charts
 const restore_getset = (property, value) => f => c => {
     if(Array.isArray(c))
         c.forEach(restore_getset(property, value)(f));
@@ -11,5 +17,7 @@ const restore_getset = (property, value) => f => c => {
     return c;
 };
 
+// specifically, turn off transitions for a chart or charts
 const no_transitions = restore_getset('transitionDuration', 0);
+// specifically, turn off transitions for a chart or charts and redraw
 const redraw_chart_no_transitions = no_transitions(c => c.redraw());
