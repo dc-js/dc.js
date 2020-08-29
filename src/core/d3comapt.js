f9680fb99c518370932097ba7ba527dd9bb35daf
Deepak Kumar <deepak@kreatio.com>
2020-08-29 10:05:02 +0530

    d3@v6 compat for `d3.nest`

--- src/core/d3comapt.js
+++ src/core/d3comapt.js
@@ -1,4 +1,6 @@
 import {event} from 'd3-selection';
+import {nest} from 'd3-collection';
+import {groups} from 'd3-array';
 
 // d3v6 has changed the arguments for event handlers.
 // We are creating a wrapper which detects if the first argument is an event, which indicated d3@v6
@@ -14,5 +16,42 @@ export function cpt (handler) {
             // older d3 - a is __data__, event from global d3.event
             handler.call(this, a, event);
         }
+    }
+}
+
+function _d3v5Nester ({key, sortKeys, sortValues, entries}) {
+    const nester = nest().key(key);
+    if (sortKeys) {
+        nester.sortKeys(sortKeys);
+    }
+    if (sortValues) {
+        nester.sortValues(sortValues);
+    }
+    return nester.entries(entries);
+}
+
+function _d3v6Nester ({key, sortKeys, sortValues, entries}) {
+    if (sortValues) {
+        entries = [...entries].sort(sortValues);
+    }
+    let out = groups(entries, key);
+    if (sortKeys) {
+        out = out.sort(sortKeys);
+    }
+
+    // remap to d3@v5 structure
+    return out.map(e => ({
+        key: `${e[0]}`, // d3@v5 always returns key as string
+        values: e[1]
+    }));
+}
+
+export function compatNestHelper ({key, sortKeys, sortValues, entries}) {
+    if (groups) {
+        // d3@v6
+        return _d3v6Nester({key, sortKeys, sortValues, entries});
+    } else {
+        // older d3
+        return _d3v5Nester({key, sortKeys, sortValues, entries});
     }
 }
