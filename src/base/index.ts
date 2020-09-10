8c26d0fbdc741c20c3e6a0ebd558ef6bd0600e8d
Deepak Kumar <deepak@kreatio.com>
2020-09-10 20:15:00 +0530

    Simplifying `index.ts` files

---
+++ src/base/index.ts
@@ -0,0 +1,17 @@
+export * from './colors/index'
+
+export * from './d3.box';
+
+export * from './i-base-mixin-conf';
+export * from './i-bubble-mixin-conf';
+export * from './i-color-mixin-conf';
+export * from './i-coordinate-grid-mixin-conf';
+export * from './i-margin-mixin-conf';
+export * from './i-stack-mixin-conf';
+
+export * from './base-mixin';
+export * from './bubble-mixin';
+export * from './color-mixin';
+export * from './coordinate-grid-mixin';
+export * from './margin-mixin';
+export * from './stack-mixin';
---
+++ src/compat/base/index.ts
@@ -0,0 +1,9 @@
+export * from './d3.box';
+
+export * from './base-mixin';
+export * from './bubble-mixin';
+export * from './cap-mixin';
+export * from './color-mixin';
+export * from './coordinate-grid-mixin';
+export * from './margin-mixin';
+export * from './stack-mixin';
