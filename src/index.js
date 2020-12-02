6f92104d3d0e76c1b8fbd7e494e718f4d1c8f576
Deepak Kumar <deepak@kreatio.com>
2020-12-02 10:55:49 +0530

    Export dc compat functions

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ export * from './core/logger';
 export * from './core/printers';
 export * from './core/units';
 export * from './core/utils';
+export * from './core/d3compat';
 
 export * from './base/base-mixin';
 export * from './base/bubble-mixin';
