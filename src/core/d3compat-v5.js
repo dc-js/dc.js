dbb7542df6429306cd82c0055249a48897a05862
Patrik Kullman <patrik.kullman@gmail.com>
2021-04-05 00:04:51 +0200

    fix: eventHandler
    Misleading comments/docs, both eventHandler versions are used in d3@v6

--- src/core/d3compat-v5.js
+++ src/core/d3compat-v5.js
@@ -10,7 +10,13 @@ const majorVer = +version[0];
 if (majorVer < 6) {
     Object.assign(config._d3compat, {
         eventHandler: handler => function eventHandler (a ,b) {
-            handler.call(this, a, event);
+            if (a && a.target) {
+                // d3@v6 - b is __data__, a is the event
+                handler.call(this, b, a);
+            } else {
+                // older d3 - a is __data__, event from global d3.event
+                handler.call(this, a, event);
+            }
         },
         nester: ({key, sortKeys, sortValues, entries}) => {
             const nester = nest().key(key);
--- src/core/d3compat-v6.js
+++ src/core/d3compat-v6.js
@@ -9,8 +9,14 @@ const majorVer = +version[0];
 
 if (majorVer > 5) {
     Object.assign(config._d3compat, {
-        eventHandler: handler => function eventHandler (a, b) {
-            handler.call(this, b, a);
+        eventHandler: handler => function (a, b) {
+            if (a && a.target) {
+                // d3@v6 - b is __data__, a is the event
+                handler.call(this, b, a);
+            } else {
+                // older d3 - a is __data__, event from global d3.event
+                handler.call(this, a);
+            }
         },
         nester: ({key, sortKeys, sortValues, entries}) => {
             if (sortValues) {
