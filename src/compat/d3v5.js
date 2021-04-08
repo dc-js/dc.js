56086a4574c6d090aa6618794f3153ac7b8dd973
Gordon Woodhull <gordon@woodhull.com>
2021-04-08 06:55:45 -0400

    Revert "fix: eventHandler"
    We are still using fake events with d3@5 method signature in our tests, which is why
    this workaround was needed.
    
    Fixing the tests instead!
    
    This reverts commit add89b1c23129286d839515aaba80ff7e9662973.

--- src/compat/d3v5.js
+++ src/compat/d3v5.js
@@ -9,14 +9,8 @@ const majorVer = +version[0];
 
 if (majorVer < 6) {
     Object.assign(d3compat, {
-        eventHandler: handler => function eventHandler (a ,b) {
-            if (a && a.target) {
-                // d3@v6 - b is __data__, a is the event
-                handler.call(this, b, a);
-            } else {
-                // older d3 - a is __data__, event from global d3.event
-                handler.call(this, a, event);
-            }
+        eventHandler: handler => function eventHandler (a, b) {
+            handler.call(this, a, event);
         },
         nester: ({key, sortKeys, sortValues, entries}) => {
             const nester = nest().key(key);
