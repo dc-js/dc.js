a2ca6cde0be22267fd0705901ac70158ca9fd8b2
kleczekr <kleczek.r@gmail.com>
2022-09-06 23:58:33 +0200

    Update examples to use the new Bootstrap version.

--- web-src/examples/header.js
+++ web-src/examples/header.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
     document.getElementById(
         'header'
-    ).innerHTML = `<a href="//dc-js.github.io/dc.js"><img src="../dc.logo.png" style="float:left; padding-right: 1em" width=50 height=50></img></a>
+    ).innerHTML = `<a href="//dc-js.github.io/dc.js"><img src="../dc.logo.png" style="float:left; margin-right: 1em" width=50 height=50></img></a>
 <div id="links" style="padding:10px 0px 0px 10px">
     <a href=".">all ${dir}</a>
     &emsp;
